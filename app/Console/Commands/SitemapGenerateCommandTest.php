b2ec0466d0f353ad02c18c8d5013414edb8fa8d5
Benjamin Crozat <hello@benjamincrozat.com>
2022-09-16 01:00:30 +0200

    Added sitemap generation

---
+++ app/Console/Commands/SitemapGenerateCommandTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Post;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
+
+class SitemapGenerateCommandTest extends TestCase
+{
+    public function test_it_generates_a_sitemap() : void
+    {
+        Artisan::call(SitemapGenerateCommand::class);
+
+        $content = file_get_contents(public_path('/sitemap.xml'));
+
+        $this->assertStringContainsString(route('home'), $content);
+
+        Post::all()->each(function (Post $post) use ($content) {
+            $this->assertStringContainsString(route('posts.show', $post->slug), $content);
+        });
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,13 +3,14 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Console\Commands\SitemapGenerateCommand;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule) : void
     {
-        //
+        $schedule->command(SitemapGenerateCommand::class)->daily();
     }
 
     protected function commands() : void
