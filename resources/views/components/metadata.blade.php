086081128d66b9f0015e12c2c6fe8182b0e33b00
Benjamin Crozat <hello@benjamincrozat.com>
2022-09-18 15:44:30 +0200

    Updated UI

--- resources/views/components/metadata.blade.php
+++ resources/views/components/metadata.blade.php
@@ -2,11 +2,12 @@
     <img src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}" width="20" height="20" alt="Benjamin Crozat's avatar." class="-translate-y-[.5px] rounded-full" />
 
     <span>
-        <span class="text-black">Benjamin Crozat</span>
-        on
+        <span class="text-black">Benjamin Crozat</span>,
+
         <time datetime="{{ $date->toDateString() }}" class="text-black">
             {{ $date->isoFormat('LL') }}
         </time>
+
         —&nbsp;@choice(':count&nbsp;minute&nbsp;read|:count&nbsp;minutes&nbsp;read', $readTime)
     </span>
 </p>
