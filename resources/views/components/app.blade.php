339ff7f432d719ff0ba2e19bebf0ee969079bba8
Benjamin Crozat <hello@benjamincrozat.com>
2022-10-05 16:29:00 +0200

    WIP

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -4,6 +4,13 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta name="description" content="{{ $description ?? '' }}" />
+        <meta property="og:title" content="{{ $title ?? config('app.name') }}" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="{{ URL::current() }}" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@benjamincrozat" />
+        <meta name="twitter:description" content="{{ $description ?? '' }}" />
+        <meta name="twitter:title" content="{{ $title ?? config('app.name') }}" />
 
         <title>{{ $title ?? "Benjamin Crozat's blog on everything PHP and Laravel" }}</title>
 
