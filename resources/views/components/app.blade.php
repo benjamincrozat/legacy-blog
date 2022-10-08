f960c2656f38a98ed0be9126a4bfa54ef476bfb9
Benjamin Crozat <hello@benjamincrozat.com>
2022-10-08 02:09:11 +0200

    WIP

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -21,9 +21,11 @@
 
         <x-feed-links />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+
+        <link rel="canonical" href="{{ url()->current() }}" />
 
         @if (app()->isProduction())
             <script defer src="https://save-tonight-hey-jude.benjamincrozat.com/script.js" data-site="{{ config('services.fathom.site_id') }}"></script>
