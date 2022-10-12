e0ec2926d7ed41ace7e555744960c4ac31c662e0
Benjamin Crozat <hello@benjamincrozat.com>
2022-10-12 17:16:45 +0200

    WIP

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -7,7 +7,7 @@
     </h4>
 
     <x-form method="POST" action="{{ route('subscribe') }}" class="flex gap-2 mt-8 relative">
-        <input type="email" name="email" id="email" placeholder="homer@simpson.com" required class="border-0 placeholder-gray-300 pl-6 pr-32 py-3 rounded-full w-full" x-ref="newsletter" />
+        <input type="email" name="email" id="email" placeholder="homersimpson@example.com" required class="border-0 placeholder-gray-300 pl-6 pr-32 py-3 rounded-full w-full" x-ref="newsletter" />
 
         <button type="submit" class="absolute top-1/2 -translate-y-1/2 right-0 bg-blue-400 hover:bg-blue-300 flex-shrink-0 font-normal h-full px-6 rounded-full text-sm text-white transition-colors">
             Sign me up
