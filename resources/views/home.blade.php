5561a1b1c9a3bc53f502d12b1bdc7044235b61c3
Benjamin Crozat <benjamincrozat@me.com>
2022-10-20 09:06:25 +0200

    Removed section header on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,30 +3,17 @@
 >
     <x-head />
 
-    <section class="container mt-16">
-        <header class="text-center">
-            <h2 class="font-semibold font-serif sm:text-lg md:text-xl">
-                Improve your PHP & Laravel skills in 2022
-            </h2>
-
-            <p class="font-serif max-w-screen-sm mt-2 mx-auto text-sm sm:text-base">
-                The best PHP developers started their career thanks to the power of knowledge&nbsp;sharing&nbsp;of&nbsp;the&nbsp;Internet.
-            </p>
-        </header>
-
-        @if ($posts->isNotEmpty())
-            <ul class="border-t grid gap-12 mt-8 sm:mt-16 pt-8 sm:pt-16">
-                @foreach ($posts as $post)
-                    <li><x-post :post="$post" /></li>
-                @endforeach
-            </ul>
-        @endif
-    </section>
+    @if ($posts->isNotEmpty())
+        <ul class="grid gap-12 mt-8 sm:mt-16">
+            @foreach ($posts as $post)
+                <li><x-post :post="$post" /></li>
+            @endforeach
+        </ul>
+    @endif
 
     <div class="bg-gray-100 mt-8 sm:mt-16 py-8 sm:py-16">
         <x-newsletter class="container max-w-screen-sm scroll-mt-8 sm:scroll-mt-16" />
     </div>
 
-    {{-- About --}}
     <x-about />
 </x-app>
