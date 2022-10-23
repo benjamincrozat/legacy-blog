be2be2cd323ba81f63d2a7bfd5955eb75adcb7b5
Benjamin Crozat <benjamincrozat@me.com>
2022-10-23 20:01:35 +0200

    Updated favicons

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -21,9 +21,11 @@
 
         <x-feed-links />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('/img/apple-touch-icon.png') }}" />
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('/img/favicon-16x16.png') }}" />
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset('/img/favicon-32x32.png') }}" />
+        <link rel="icon" type="image/png" sizes="48x48" href="{{ secure_asset('/img/favicon-48x48.png') }}" />
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ secure_asset('/img/favicon-96x96.png') }}" />
 
         <link rel="canonical" href="{{ url()->current() }}" />
 
