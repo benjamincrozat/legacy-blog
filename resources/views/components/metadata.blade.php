39a086002777b24bfbe16c2629c6784e0f786432
Benjamin Crozat <benjamincrozat@me.com>
2022-10-25 15:19:58 +0200

    Improved author's name appearance

--- resources/views/components/metadata.blade.php
+++ resources/views/components/metadata.blade.php
@@ -2,7 +2,7 @@
     <img src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}" width="20" height="20" alt="Benjamin Crozat's avatar." class="-translate-y-[.5px] rounded-full" />
 
     <p>
-        <a href="https://twitter.com/benjamincrozat" target="_blank" rel="noopener noreferrer" class="font-normal text-black" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">Benjamin Crozat</a>
+        <a href="https://twitter.com/benjamincrozat" target="_blank" rel="noopener noreferrer" class="border-b border-blue-200 text-blue-400" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">Benjamin Crozat</a>
 
         —
 
