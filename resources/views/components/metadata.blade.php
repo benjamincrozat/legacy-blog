e4f0b6ab8b2d33008d92a344499763779d683006
Benjamin Crozat <benjamincrozat@me.com>
2022-10-28 10:52:59 +0200

    Shorter metadata

--- resources/views/components/metadata.blade.php
+++ resources/views/components/metadata.blade.php
@@ -1,17 +1,15 @@
 <div {{ $attributes->merge(['class' => 'flex items-center gap-2 text-sm']) }}>
-    <img src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}" width="20" height="20" alt="Benjamin Crozat's avatar." class="-translate-y-[.5px] rounded-full" />
+    <img src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}" width="18" height="18" alt="Benjamin Crozat's avatar." class="-translate-y-[.5px] rounded-full" />
 
     <p>
         <a href="https://twitter.com/benjamincrozat" target="_blank" rel="noopener noreferrer" class="border-b border-blue-200 text-blue-400" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">Benjamin Crozat</a>
 
         —
 
-        Published on
-
         <time datetime="{{ $publishedAt?->toDateString() }}" class="text-black">
-            {{ $publishedAt?->isoFormat('LL') }}
+            {{ $publishedAt?->isoFormat('ll') }}
         </time>
 
-        —&nbsp;@choice(':count&nbsp;minute&nbsp;read|:count&nbsp;minutes&nbsp;read', $readTime)
+        —&nbsp;@choice(':count&nbsp;min|:count&nbsp;mins', $readTime)
     </p>
 </div>
