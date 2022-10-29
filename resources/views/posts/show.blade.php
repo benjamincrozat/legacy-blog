22d0488dcec64e5ad54579b253f93bb715f1d018
Benjamin Crozat <benjamincrozat@me.com>
2022-10-29 15:41:42 +0200

    WIP

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -40,11 +40,17 @@
             </h1>
 
             {{-- Metadata --}}
-            <x-metadata
-                :published-at="$post->getPublishedAtDate()"
-                :read-time="$post->getReadTime()"
-                class="mt-4"
-            />
+            <div class="flex items-center gap-2 mt-4 text-sm">
+                <img src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}" width="18" height="18" alt="Benjamin Crozat's avatar." class="-translate-y-[.5px] rounded-full" />
+
+                <p>
+                    <a href="https://twitter.com/benjamincrozat" target="_blank" rel="noopener noreferrer" class="font-normal" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">Benjamin Crozat</a>
+
+                    <span class="mx-1">—</span>
+
+                    @choice(':count minute|:count minutes', $post->getReadTime()) read
+                </p>
+            </div>
 
             <x-table-of-contents :post="$post" />
 
