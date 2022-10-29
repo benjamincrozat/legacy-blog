bd7338d8dfa18a1a85b1a75c05b7e23cd1e534da
Benjamin Crozat <benjamincrozat@me.com>
2022-10-29 19:08:34 +0200

    WIP

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,5 +1,5 @@
 <nav {{ $attributes->merge(['class' => 'text-sm']) }}>
-    <ul class="flex items-center gap-3">
+    <ul class="flex items-center gap-2 sm:gap-3">
         <li>
             <a href="{{ route('home') }}" class="text-indigo-400" @click="window.fathom?.trackGoal('I4R3OAAU', 0)">
                 <x-heroicon-o-home class="-translate-y-px w-3 h-3" />
