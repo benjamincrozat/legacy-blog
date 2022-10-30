8d99f205c765aeed068b49184c755cd80d5e19c9
Benjamin Crozat <benjamincrozat@me.com>
2022-10-30 09:52:44 +0100

    WIP

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,9 +3,9 @@
     description="Have you ever had a question about PHP, Laravel or anything related to its ecosystem? This is the best blog to find your answer."
     class="text-gray-600"
 >
-    <div class="container sm:flex sm:items-center sm:justify-between mt-8 text-center sm:text-left">
+    <div class="container flex justify-between mt-8">
         <a href="{{ route('home') }}">
-            <span class="font-extrabold translate-y-px text-sm sm:text-base tracking-widest uppercase">
+            <span class="font-extrabold translate-y-px text-base tracking-widest uppercase">
                 Benjamin Crozat
             </span>
 
@@ -14,7 +14,7 @@
             </span>
         </a>
 
-        <nav class="flex items-center justify-center sm:justify-start gap-8 mt-8 sm:mt-0">
+        <nav class="flex items-center gap-8">
             <x-hire-me />
         </nav>
     </div>
