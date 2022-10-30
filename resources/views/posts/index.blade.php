a0ec38daeba4d0fef52f9caa6af98ffab2d6887e
Benjamin Crozat <benjamincrozat@me.com>
2022-10-30 19:25:23 +0100

    WIP

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -43,6 +43,18 @@
             </div>
         @endif
 
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3461630254419592" crossorigin="anonymous"></script>
+
+        <ins
+            class="adsbygoogle"
+            style="display:block"
+            data-ad-format="autorelaxed"
+            data-ad-client="ca-pub-3461630254419592"
+            data-ad-slot="3016389119"
+        ></ins>
+
+        <script>(adsbygoogle = window.adsbygoogle || []).push({})</script>
+
         <p class="font-bold mb-4 mt-16 text-center text-xl">
             Latest posts
         </p>
