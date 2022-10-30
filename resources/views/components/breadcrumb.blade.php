e792f44162b7b75ac68aa2b2e53ef20f16c57bc8
Benjamin Crozat <benjamincrozat@me.com>
2022-10-30 13:18:03 +0100

    WIP

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -2,7 +2,7 @@
     <ul class="flex items-center gap-2 sm:gap-3">
         <li>
             <a href="{{ route('home') }}" class="text-indigo-400" @click="window.fathom?.trackGoal('I4R3OAAU', 0)">
-                <x-heroicon-o-home class="-translate-y-px w-3 h-3" />
+                <x-heroicon-o-home class="w-3 h-3" />
                 <span class="sr-only">Home</span>
             </a>
         </li>
