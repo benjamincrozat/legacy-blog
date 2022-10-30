e70f65b545f7acf465a79fa4fcc3cd7b0befdf26
Benjamin Crozat <benjamincrozat@me.com>
2022-10-30 10:08:30 +0100

    WIP

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-gradient-to-r from-white to-gray-50/30 p-4 sm:p-5 rounded-lg shadow-lg shadow-gray-200">
     <div class="flex items-center gap-2 text-sm">
-        <img src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}" width="18" height="18" alt="Benjamin Crozat" class="-translate-y-[.5px] rounded-full" />
+        <img src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}" width="18" height="18" alt="Benjamin Crozat" class="rounded-full" />
 
         <p>
             <a href="https://twitter.com/benjamincrozat" target="_blank" rel="noopener noreferrer" class="font-semibold" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">Benjamin Crozat</a>
