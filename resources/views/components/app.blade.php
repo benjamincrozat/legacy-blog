b16e0f39ee0190737c05d40337e2f91f2e340577
Benjamin Crozat <benjamincrozat@me.com>
2022-10-30 19:28:19 +0100

    WIP

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -45,6 +45,8 @@
         @if (app()->isProduction())
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-X7RCWQHXPL"></script>
             <script>window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments) } gtag('js', new Date()); gtag('config', 'G-X7RCWQHXPL')</script>
+
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3461630254419592" crossorigin="anonymous"></script>
         @endif
     </body>
 </html>
