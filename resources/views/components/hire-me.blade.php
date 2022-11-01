350ce6763fb54bca85f8b89b3cb15d83fe319924
Benjamin Crozat <benjamincrozat@me.com>
2022-11-01 02:58:19 +0100

    WIP

--- resources/views/components/hire-me.blade.php
+++ resources/views/components/hire-me.blade.php
@@ -1,3 +1,7 @@
-<a href="{{ route('home') }}" {{ $attributes->merge(['class' => 'bg-gradient-to-r from-indigo-300 to-indigo-400 font-normal px-3 py-2 rounded shadow-md text-white tracking-widest text-xs uppercase']) }}>
+<a
+    href="{{ route('home') }}"
+    {{ $attributes->merge(['class' => 'bg-gradient-to-r from-indigo-300 to-indigo-400 font-normal px-3 py-2 rounded shadow-md text-white tracking-widest text-xs uppercase']) }}
+    @click="window.fathom?.trackGoal('SJKONFQC', 0)"
+>
     Hire me!
 </a>
