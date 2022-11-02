19b435fda923dd6d3fcf873ade2899ae07329997
Benjamin Crozat <benjamincrozat@me.com>
2022-11-02 17:12:10 +0100

    WIP

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -83,7 +83,7 @@
 
             @if ($post->hideBanners)
                 <div class="border mt-8 p-4 rounded text-xs">
-                    <p>Sometimes, the blog uses affiliate links, which can compensate me at no cost to you if you decide to pursue the deal.</p>
+                    <p>Sometimes, I use affiliate links, which can compensate me at no cost to you if you decide to pursue the deal.</p>
                     <p class="mt-2">I only promote products I've personally used and stand behind.</p>
                 </div>
             @endif
