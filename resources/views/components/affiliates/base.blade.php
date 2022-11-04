a1e7c5887328ed74b9cfe91718a53836f0113f16
Benjamin Crozat <benjamincrozat@me.com>
2022-11-04 23:56:45 +0100

    Update base.blade.php

--- resources/views/components/affiliates/base.blade.php
+++ resources/views/components/affiliates/base.blade.php
@@ -10,7 +10,7 @@
         <p class="mt-6">
             <a
                 href="{{ route('affiliate', $button->attributes->get('slug')) }}"
-                class="bg-gradient-to-r from-indigo-300 dark:from-indigo-400 to-indigo-400 dark:to-indigo-500 block leading-tight sm:max-w-screen-xs mt-2 mx-auto px-4 py-3 rounded shadow-md text-center text-green-50"
+                class="bg-gradient-to-r from-indigo-300 dark:from-indigo-500 to-indigo-400 dark:to-indigo-600 block leading-tight sm:max-w-screen-xs mt-2 mx-auto px-4 py-3 rounded shadow-md text-center text-green-50"
                 @click="window.fathom?.trackGoal('K8DBWLRF', 0)"
             >
                 {{ $button }}
