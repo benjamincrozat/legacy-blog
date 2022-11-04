e9991d89b5436ad059f33951a93a94e80597c803
Benjamin Crozat <benjamincrozat@me.com>
2022-11-04 21:10:50 +0100

    WIP

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -40,7 +40,7 @@
             Latest posts
         </h2>
 
-        <x-affiliates.cloudways />
+        <x-dynamic-component :component="$affiliates->shuffle()->first()" />
 
         @if ($posts->isNotEmpty())
             <div class="grid sm:grid-cols-2 gap-4 sm:gap-8 mt-8">
