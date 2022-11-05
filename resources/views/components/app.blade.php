d69bcca2d8cc90cc8fbd1574448c5a16c303bfb6
Benjamin Crozat <benjamincrozat@me.com>
2022-11-05 13:39:20 +0100

    WIP

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <meta name="ir-site-verification-token" value="1841852303" />
         <meta name="description" content="{{ $description ?? '' }}" />
         <meta property="og:title" content="{{ $title ?? config('app.name') }}" />
         @if (! empty($image))
