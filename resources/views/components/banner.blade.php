97e953ec2718b7c027f4f9eda0db3beb45535d80
Benjamin Crozat <benjamincrozat@me.com>
2022-11-06 15:12:45 +0100

    WIP

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,6 +1,6 @@
 @if (! empty($banner))
     <div {{ $attributes->merge(['class' => 'border dark:border-gray-800 rounded']) }}>
-        <div class="border-b dark:border-gray-800 flex items-center justify-center gap-2 font-normal leading-tight p-2 text-indigo-400">
+        <div class="border-b dark:border-gray-800 flex items-center justify-center gap-2 font-normal leading-tight p-2 text-emerald-500">
             <x-heroicon-o-information-circle class="-translate-y-[.5px] flex-shrink-0 w-4 h-5 h-4" />
             {{ $banner->title }}
         </div>
