b4d5d73350dc3a6dc1a0c384e25f83317b3d275b
Benjamin Crozat <benjamincrozat@me.com>
2022-11-06 21:39:45 +0100

    WIP

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -5,6 +5,7 @@ namespace App\Nova\Dashboards;
 use Laravel\Nova\Dashboard;
 use App\Nova\Metrics\PostsIntent;
 use App\Nova\Metrics\AffiliateClicks;
+use App\Nova\Metrics\AffiliatesCount;
 use App\Nova\Metrics\SubscribersCount;
 
 class Main extends Dashboard
@@ -13,6 +14,7 @@ class Main extends Dashboard
     {
         return [
             new PostsIntent,
+            new AffiliatesCount,
             new AffiliateClicks,
             new SubscribersCount,
         ];
