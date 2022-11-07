404f4d239214fd40857642cb54f5ceb54ab35622
Benjamin Crozat <benjamincrozat@me.com>
2022-11-07 14:14:12 +0100

    WIP

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,25 +49,31 @@
 
             <x-blog.toc :toc="$post->getTableOfContents()" class="mb-8" />
 
-            <div class="border dark:border-gray-800 mb-8 p-4 rounded">
-                <p class="font-normal">
-                    Let me share with you my discoveries about the art of crafting websites, <span class="text-indigo-400">for&nbsp;free</span>.
-                </p>
+            <div class="border dark:border-gray-800 rounded">
+                <h4 class="font-bold py-2 text-center">
+                    Newsletter
+                </h4>
 
-                <x-form method="POST" action="{{ route('subscribe') }}" class="mt-4">
-                    <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="homer@simpson.com"
-                        required
-                        class="dark:bg-gray-800 block dark:border-0 border-gray-200 placeholder-gray-200 dark:placeholder-gray-600 rounded text-sm w-full"
-                    />
+                <div class="border-t dark:border-gray-800 p-4">
+                    <p class="font-normal">
+                        Let me share with you my discoveries about the art of crafting websites, <span class="text-indigo-400">for&nbsp;free</span>.
+                    </p>
 
-                    <button type="submit" class="bg-gradient-to-r from-purple-300 dark:from-purple-700 to-purple-400 dark:to-purple-800 block font-bold mt-4 px-3 py-2 rounded shadow-md text-center text-purple-50 w-full">
-                        Sign me up!
-                    </button>
-                </x-form>
+                    <x-form method="POST" action="{{ route('subscribe') }}" class="mt-4">
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="homer@simpson.com"
+                            required
+                            class="dark:bg-gray-800 block dark:border-0 border-gray-200 placeholder-gray-200 dark:placeholder-gray-600 rounded text-sm w-full"
+                        />
+
+                        <button type="submit" class="bg-gradient-to-r from-purple-300 dark:from-purple-700 to-purple-400 dark:to-purple-800 block font-bold mt-4 px-3 py-2 rounded shadow-md text-center text-purple-50 w-full">
+                            Sign me up!
+                        </button>
+                    </x-form>
+                </div>
             </div>
 
             @if ($post->promotes_affiliate_links)
