3a93bdbfea5f91238310a05d8a87e9a63e87af4e
Benjamin Crozat <benjamincrozat@me.com>
2022-11-08 03:26:15 +0100

    WIP

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -27,7 +27,7 @@
                             />
                         </a>
 
-                        <a href="{{ route('posts.show', $post->slug) }}" class="bg-gray-900 dark:bg-gradient-to-r from-white dark:from-gray-800 to-gray-50/30 dark:to-gray-700/50 block leading-tight mt-2 p-3 rounded-md text-sm text-white dark:text-current" @click="window.fathom?.trackGoal('OKJIR46O', 0)">
+                        <a href="{{ route('posts.show', $post->slug) }}" class="bg-gray-900 dark:bg-gradient-to-r from-white dark:from-gray-800/50 to-gray-50/30 dark:to-gray-800/50 block leading-tight mt-2 p-3 rounded-md text-sm text-white dark:text-current" @click="window.fathom?.trackGoal('OKJIR46O', 0)">
                             <figcaption class="flex items-center justify-between gap-4">
                                 <span class="truncate">{{ $post->title }}</span>
                                 <x-heroicon-o-arrow-right class="flex-shrink-0 w-3 h-3" />
