b574f5d7cf2a2ae209a47f74b4df1715c0290684
Benjamin Crozat <benjamincrozat@me.com>
2022-11-11 09:06:57 +0100

    WIP

--- resources/views/components/deal.blade.php
+++ resources/views/components/deal.blade.php
@@ -1,30 +1,24 @@
 @if (! empty($deal))
-    <div {{ $attributes->merge(['class' => 'border dark:border-gray-800 rounded']) }}>
-        <div class="border-b dark:border-gray-800 flex items-center justify-center gap-2 font-normal leading-tight p-2 text-emerald-500">
-            <x-heroicon-o-information-circle class="-translate-y-[.5px] flex-shrink-0 w-4 h-5 h-4" />
-            {{ $deal->affiliate->name }}
-        </div>
+    <div {{ $attributes->merge(['class' => 'border dark:border-gray-800 flex flex-col gap-2 p-4 rounded']) }}>
+        <div class="flex flex-grow items-center justify-between gap-8">
+            <div>
+                <p class="font-bold">{{ $deal->affiliate->name }}</p>
 
-        <div class="grid gap-4 p-4">
-            <div class="grid place-items-center gap-4 md:max-w-screen-sm md:mx-auto">
-                @if ($deal->image)
-                    <img src="{{ str_replace('w_auto', 'h_110', $deal->image) }}" width="55" height="55" alt="{{ $deal->affiliate->name }}" class="rounded-lg" />
-                @endif
-
-                <div>
+                <div class="mt-2 text-sm">
                     {!! Illuminate\Support\Str::lightdown($deal->content) !!}
                 </div>
             </div>
 
-            <div>
-                <a
-                    href="{{ route('affiliate', $deal->affiliate) }}"
-                    class="bg-gradient-to-r from-emerald-400 dark:from-emerald-700 to-emerald-500 dark:to-emerald-800 block leading-tight px-4 py-3 rounded-sm shadow-md text-center text-white"
-                    @click="window.fathom?.trackGoal('K8DBWLRF', 0)"
-                >
-                    {!! Illuminate\Support\Str::lightdown($deal->button) !!}
-                </a>
-            </div>
+            @if ($deal->image)
+                <img src="{{ str_replace('w_auto', 'h_110', $deal->image) }}" width="55" height="55" alt="{{ $deal->affiliate->name }}" class="rounded-lg h-12 sm:h-auto" />
+            @endif
         </div>
+
+        <a
+            href="{{ route('affiliate', $deal->affiliate) }}"
+            class="bg-gradient-to-r from-emerald-400 dark:from-emerald-700 to-emerald-500 dark:to-emerald-800 block leading-tight mt-2 px-4 py-3 rounded-sm shadow-md text-center text-sm text-white"
+        >
+            {!! Illuminate\Support\Str::lightdown($deal->button) !!}
+        </a>
     </div>
 @endif
