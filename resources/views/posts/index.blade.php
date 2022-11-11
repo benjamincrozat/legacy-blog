415924171dfbfed150dac693f64974062b9673ed
Benjamin Crozat <benjamincrozat@me.com>
2022-11-11 09:12:13 +0100

    WIP

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -31,28 +31,7 @@
         @if ($deals->isNotEmpty())
             <div class="grid md:grid-cols-2 gap-4 mt-8">
                 @foreach ($deals as $deal)
-                    <div class="border dark:border-gray-800 flex flex-col gap-2 p-4 rounded">
-                        <div class="flex flex-grow items-center justify-between gap-8">
-                            <div>
-                                <p class="font-bold">{{ $deal->affiliate->name }}</p>
-
-                                <div class="mt-2 text-sm">
-                                    {!! Illuminate\Support\Str::lightdown($deal->content) !!}
-                                </div>
-                            </div>
-
-                            @if ($deal->image)
-                                <img src="{{ str_replace('w_auto', 'h_110', $deal->image) }}" width="55" height="55" alt="{{ $deal->affiliate->name }}" class="rounded-lg h-12 sm:h-auto" />
-                            @endif
-                        </div>
-
-                        <a
-                            href="{{ route('affiliate', $deal->affiliate) }}"
-                            class="bg-gradient-to-r from-emerald-400 dark:from-emerald-700 to-emerald-500 dark:to-emerald-800 block leading-tight mt-2 px-4 py-3 rounded-sm shadow-md text-center text-sm text-white"
-                        >
-                            {!! Illuminate\Support\Str::lightdown($deal->button) !!}
-                        </a>
-                    </div>
+                    <x-deal :deal="$deal" />
                 @endforeach
             </div>
         @endif
