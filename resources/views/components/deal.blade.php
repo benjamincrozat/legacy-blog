0cb56d6a62913403a2d385192071f438f1508c47
Benjamin Crozat <benjamincrozat@me.com>
2022-11-11 09:51:31 +0100

    WIP

--- resources/views/components/deal.blade.php
+++ resources/views/components/deal.blade.php
@@ -1,8 +1,16 @@
 @if (! empty($deal))
-    <div {{ $attributes->merge(['class' => 'border dark:border-gray-800 flex flex-col gap-2 p-4 rounded']) }}>
+    <div {{ $attributes->merge([
+        'class' => 'border dark:border-gray-800 flex flex-col gap-2 p-4 rounded'
+    ]) }}>
         <div class="flex flex-grow items-center justify-between gap-8">
             <div>
-                <p class="font-bold">{{ $deal->affiliate->name }}</p>
+                <p>
+                    <span class="font-bold @if ($deal->end_at) text-orange-400 @endif">{{ $deal->affiliate->name }}</span>
+
+                    @if ($deal->end_at)
+                        <span class="bg-gradient-to-r from-orange-300 to-orange-400 inline-block ml-2 px-3 py-1 rounded-full text-white text-xs">Deal ends in <span class="font-bold">@choice(':count day|:count days', $deal->end_at->diffInDays())</span></span>
+                    @endif
+                </p>
 
                 <div class="mt-2 text-sm">
                     {!! Illuminate\Support\Str::lightdown($deal->content) !!}
@@ -16,7 +24,7 @@
 
         <a
             href="{{ route('affiliate', $deal->affiliate) }}"
-            class="bg-gradient-to-r from-emerald-400 dark:from-emerald-700 to-emerald-500 dark:to-emerald-800 block leading-tight mt-2 px-4 py-3 rounded-sm shadow-md text-center text-sm text-white"
+            class="bg-gradient-to-r @if ($deal->end_at) from-orange-300 to-orange-400 @else from-emerald-400 dark:from-emerald-700 to-emerald-500 dark:to-emerald-800 @endif block leading-tight mt-2 px-4 py-3 rounded-sm shadow-md text-center text-sm text-white"
             @click="window.fathom?.trackGoal('K8DBWLRF', 0)"
         >
             {!! Illuminate\Support\Str::lightdown($deal->button) !!}
