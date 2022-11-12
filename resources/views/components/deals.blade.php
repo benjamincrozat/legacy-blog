fa073b06a65e84a0604bfa6c6b70cb70efdd68e9
Benjamin Crozat <benjamincrozat@me.com>
2022-11-12 20:57:23 +0100

    WIP

--- resources/views/components/deals.blade.php
+++ resources/views/components/deals.blade.php
@@ -4,7 +4,7 @@
     </h4>
 
     @if ($deals->isNotEmpty())
-        <div class="flex md:grid md:grid-cols-2 gap-2 mt-8 px-4 md:px-0 overflow-x-scroll md:overflow-x-visible snap-x md:snap-none snap-mandatory">
+        <div class="flex md:grid md:grid-cols-2 gap-4 mt-8 px-4 md:px-0 overflow-x-scroll md:overflow-x-visible snap-x md:snap-none snap-mandatory">
             @foreach ($deals as $deal)
                 <x-deal :deal="$deal" class="flex-shrink-0 snap-start sm:snap-center md:snap-normal scroll-ml-4 md:scroll-ml-0 w-[90%] sm:w-[70%] md:w-auto" />
             @endforeach
