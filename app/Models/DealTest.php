205e163d6fce763727417616968f006e52e88b3c
Benjamin Crozat <benjamincrozat@me.com>
2022-11-12 19:36:12 +0100

    WIP

--- app/Models/DealTest.php
+++ app/Models/DealTest.php
@@ -8,7 +8,7 @@ class DealTest extends TestCase
 {
     public function test_has_active_scope() : void
     {
-        Deal::factory()->create();
+        Deal::factory()->forAffiliate()->create();
 
         Deal::factory()->create(['start_at' => null]);
 
@@ -22,7 +22,7 @@ class DealTest extends TestCase
 
     public function test_has_an_affiliate_relationship() : void
     {
-        $deal = Deal::factory()->create();
+        $deal = Deal::factory()->forAffiliate()->create();
 
         $this->assertInstanceOf(Affiliate::class, $deal->affiliate);
     }
