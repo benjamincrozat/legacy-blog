8bb1aa3d1a04452122de831bfd584eb65581bc13
Benjamin Crozat <benjamincrozat@me.com>
2022-11-13 22:06:32 +0100

    WIP

--- resources/views/components/deal.blade.php
+++ resources/views/components/deal.blade.php
@@ -1,5 +1,5 @@
 @if (! empty($deal))
-    <div {{ $attributes->except('deal')->merge([
+    <aside {{ $attributes->except('deal')->merge([
         'class' => $deal->end_at ? 'border border-orange-200 dark:border-orange-900 flex flex-col gap-2 p-4 rounded' : 'border dark:border-gray-800 flex flex-col gap-2 p-4 rounded'
     ]) }}>
         <div class="flex flex-grow items-center justify-between gap-8">
@@ -29,5 +29,5 @@
         >
             {!! Illuminate\Support\Str::lightdown($deal->button) !!}
         </a>
-    </div>
+    </aside>
 @endif
