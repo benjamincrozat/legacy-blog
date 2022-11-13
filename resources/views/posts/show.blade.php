9fcaf7d3fa22911c855a30f4ba562014f6c6485f
Benjamin Crozat <benjamincrozat@me.com>
2022-11-13 10:40:04 +0100

    WIP

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -74,7 +74,7 @@
     <x-newsletter class="container mt-16" />
 
     @if ($others->isNotEmpty())
-        <div class="container mt-16">
+        <div class="container max-w-[1024px] mt-16">
             <h4 class="font-bold text-center text-xl">Other posts to read</h4>
 
             <div class="grid md:grid-cols-2 gap-4 sm:gap-8 mt-8">
