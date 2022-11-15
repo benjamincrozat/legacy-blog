538b1c862876c35cec88b45fdf603a116dbe6189
Benjamin Crozat <benjamincrozat@me.com>
2022-11-15 19:14:03 +0100

    WIP

--- resources/views/components/blog/top.blade.php
+++ resources/views/components/blog/top.blade.php
@@ -10,11 +10,14 @@
     </div>
 
     <nav class="flex items-center gap-6 sm:gap-8">
-        @if (auth()->check() && 'benjamincrozat@me.com' === auth()->user()?->email)
-            <a href="{{ config('nova.path') }}" class="font-normal text-sm sm:text-base">
-                Nova
-            </a>
-        @endif
+        <a
+            href="{{ config('deals.index') }}"
+            class="font-normal relative text-sm sm:text-base"
+            @click="window.fathom?.trackGoal('Y4RZQNDR', 0)"
+        >
+            <span>Deals</span>
+            <span class="absolute -top-4 -right-6 bg-gradient-to-r from-orange-300 dark:from-orange-400 to-orange-400 dark:to-orange-500 font-bold inline-block leading-tight px-2 py-1 rounded-full text-white text-xs transform scale-75">New</span>
+        </a>
 
         <x-hire-me />
     </nav>
