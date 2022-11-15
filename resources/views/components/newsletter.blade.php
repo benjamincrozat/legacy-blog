d18a8c14d224bf0641137aadadbffe840b9feef5
Benjamin Crozat <benjamincrozat@me.com>
2022-11-15 17:32:25 +0100

    WIP

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -1,4 +1,4 @@
-<section {{ $attributes }}>
+<section id="newsletter" {{ $attributes }}>
     <h2 class="font-semibold sm:max-w-screen-xs mx-auto text-xl sm:text-2xl md:text-3xl text-center">
         I share everything I learn about the&nbsp;art&nbsp;of&nbsp;crafting&nbsp;websites, <span class="text-indigo-400">for&nbsp;free</span>!
     </h2>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -30,11 +30,17 @@
             <span class="opacity-75">@choice(':count minute|:count minutes', $post->read_time) read</span>
         </div>
 
-        @if ($post->introduction)
+        @if ($post->introduction || ! $post->promotes_affiliate_links)
             <div class="break-words max-w-full mt-8
             prose prose-a:border-b prose-a:border-indigo-400/50 prose-a:text-indigo-400 prose-a:no-underline
             prose-code:dark:text-current prose-headings:dark:text-white prose-hr:dark:border-gray-800 prose-thead:dark:border-gray-800 prose-strong:text-current prose-tr:dark:border-gray-800 dark:text-current">
-                {!! $post->rendered_introduction !!}
+                <p class="italic">
+                    Before you start reading this article, did you know I have a [free newsletter](#newsletter) you can subscribe to?
+                </p>
+
+                @if ($post->introduction)
+                    {!! $post->rendered_introduction !!}
+                @endif
             </div>
         @endif
 
