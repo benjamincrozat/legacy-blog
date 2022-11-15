93e982991b530dda5e1f31a665f57f5b6bdaf4ca
Benjamin Crozat <benjamincrozat@me.com>
2022-11-15 21:57:14 +0100

    WIP

--- app/Models/DealTest.php
+++ app/Models/DealTest.php
@@ -22,18 +22,21 @@ class DealTest extends TestCase
 
     public function test_has_an_highlighted_scope() : void
     {
-        $deal = Deal::factory()->forAffiliate()->create([
+        $first = Deal::factory()->forAffiliate()->create([
             'highlighted' => true,
+            'end_at' => now()->addMonth(),
+        ]);
+
+        Deal::factory(10)->forAffiliate()->create();
+
+        $second = Deal::factory()->forAffiliate()->create([
             'end_at' => now()->addWeek(),
         ]);
 
-        Deal::factory()->forAffiliate()->create([
-            'end_at' => now()->addWeek(),
-        ]);
+        $deals = Deal::active()->highlightedFirst()->get();
 
-        $firstDeal = Deal::active()->highlightedFirst()->first();
-
-        $this->assertTrue($firstDeal->is($deal));
+        $this->assertTrue($deals[0]->is($first));
+        $this->assertTrue($deals[1]->is($second));
     }
 
     public function test_has_an_affiliate_relationship() : void
