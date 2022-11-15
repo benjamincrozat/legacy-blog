97c513a3b1426351833826bd997b0d169bf9238d
Benjamin Crozat <benjamincrozat@me.com>
2022-11-15 18:00:32 +0100

    WIP

--- database/migrations/2022_11_15_170009_add_description_column_to_categories_table.php
+++ database/migrations/2022_11_15_170009_add_description_column_to_categories_table.php
@@ -7,16 +7,15 @@ use Illuminate\Database\Migrations\Migration;
 return new class extends Migration {
     public function up() : void
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug');
-            $table->timestamps();
+        Schema::table('categories', function (Blueprint $table) {
+            $table->text('description')->nullable();
         });
     }
 
     public function down() : void
     {
-        Schema::dropIfExists('categories');
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
     }
 };
