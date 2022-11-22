76b2b51bb0a2d4c4b701378a948034e3b131ecef
Benjamin Crozat <benjamincrozat@me.com>
2022-11-22 20:21:18 +0100

    WIP

--- database/migrations/2022_11_22_191715_add_image_column_to_affiliates_table.php
+++ database/migrations/2022_11_22_191715_add_image_column_to_affiliates_table.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Migrations\Migration;
 return new class extends Migration {
     public function up() : void
     {
-        Schema::table('posts', function (Blueprint $table) {
+        Schema::table('affiliates', function (Blueprint $table) {
             $table->string('image')->nullable();
         });
     }
 
     public function down() : void
     {
-        Schema::table('posts', function (Blueprint $table) {
+        Schema::table('affiliates', function (Blueprint $table) {
             $table->dropColumn('image');
         });
     }
