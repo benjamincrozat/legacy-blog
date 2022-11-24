16824461958597f93117b0d4a5c2b1b0588d919a
Benjamin Crozat <benjamincrozat@me.com>
2022-11-24 15:07:23 +0100

    WIP

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -77,7 +77,7 @@
                         <div class="bg-gradient-to-r from-white dark:from-gray-800/50 to-gray-50/30 dark:to-gray-800/50 rounded-lg shadow-lg shadow-gray-200 dark:shadow-black/10">
                             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3461630254419592" crossorigin="anonymous"></script>
 
-                            <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-g7-2g+18-3y+kz" data-ad-client="ca-pub-3461630254419592" data-ad-slot="1205956294"></ins>
+                            <ins class="adsbygoogle !h-full" style="display:block" data-ad-format="fluid" data-ad-layout-key="-g7-2g+18-3y+kz" data-ad-client="ca-pub-3461630254419592" data-ad-slot="1205956294"></ins>
 
                             <script>(adsbygoogle = window.adsbygoogle || []).push({})</script>
                         </div>
