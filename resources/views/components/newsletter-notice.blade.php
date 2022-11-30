ad58e79a45f9521793c177af422948ca1ce92c87
Benjamin Crozat <benjamincrozat@me.com>
2022-11-30 17:08:00 +0100

    WIP

--- resources/views/components/newsletter-notice.blade.php
+++ resources/views/components/newsletter-notice.blade.php
@@ -1,4 +1,9 @@
-<div {{ $attributes->merge(['class' => 'border dark:border-gray-800 italic p-4 rounded']) }}>
-    <p class="my-0">Before you start reading this article, did you know <strong class="font-bold">@choice(':count person|:count persons', $subscribersCount) subscribed to my newsletter</strong>?</p>
-    <p class="my-0"><a href="#newsletter" class="border-b border-indigo-400/50 font-bold text-indigo-400">Join them and enjoy free content</a> about the art of crafting websites!</p>
+<div {{ $attributes->merge(['class' => 'border dark:border-gray-800 italic not-prose p-4 rounded']) }}>
+    <p>
+        Before you start reading this article, did you know <strong class="font-bold">@choice(':count person|:count persons', $subscribersCount) subscribed to my newsletter</strong>?
+    </p>
+
+    <p class="mt-2">
+        <a href="#newsletter" class="border-b border-indigo-400/50 font-bold text-indigo-400">Join them and enjoy free content</a> about the art of crafting websites!
+    </p>
 </div>
