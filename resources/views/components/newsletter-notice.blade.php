413c1d1870c8db9a04be542a0aea8747413f4556
Benjamin Crozat <benjamincrozat@me.com>
2022-12-01 17:09:55 +0100

    WIP

--- resources/views/components/newsletter-notice.blade.php
+++ resources/views/components/newsletter-notice.blade.php
@@ -4,6 +4,6 @@
     </p>
 
     <p class="mt-2">
-        <a href="#newsletter" class="border-b border-indigo-400/50 font-bold text-indigo-400">Join them and enjoy free content</a> about the art of crafting websites!
+        <a href="{{ route('home') }}" class="border-b border-indigo-400/50 font-bold text-indigo-400">Join them and enjoy free content</a> about the art of crafting websites!
     </p>
 </div>
