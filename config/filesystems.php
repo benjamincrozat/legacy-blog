cebce948a73f85588862c28ff6f15018fbf41cdb
Benjamin Crozat <benjamincrozat@me.com>
2022-12-07 20:08:36 +0100

    WIP

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+
+        'dropbox' => [
+            'driver' => 'dropbox',
+            'token' => env('DROPBOX_TOKEN'),
+        ],
     ],
 
     /*
