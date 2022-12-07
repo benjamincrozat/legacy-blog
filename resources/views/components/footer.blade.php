5a753929ad6f16b8f3298ccb67dd662b67c314a2
Benjamin Crozat <benjamincrozat@me.com>
2022-12-07 15:49:16 +0100

    WIP

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -28,6 +28,10 @@
         </ul>
     </div>
 
+    <p class="mt-8 text-center">
+        <span class="opacity-50">Hosting by</span> <a href="https://benjamincrozat.com/recommends/digitalocean" target="_blank" rel="nofollow noopener noreferrer">DigitalOcean</a> <span class="opacity-50">and analytics by</span> <a href="https://benjamincrozat.com/recommends/fathom-analytics" target="_blank" rel="nofollow noopener noreferrer">Fathom</a><span class="opacity-50">.</span>
+    </p>
+
     <p class="mt-8 opacity-50 text-center text-xs tracking-widest uppercase">
         © Benjamin Crozat {{ date('Y') }}. All&nbsp;rights&nbsp;reserved.
     </p>
