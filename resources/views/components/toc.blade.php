4b2b1f378c46d7f776e23773c4e50c2130f18539
Benjamin Crozat <benjamincrozat@me.com>
2022-12-14 18:32:54 +0100

    WIP

--- resources/views/components/toc.blade.php
+++ resources/views/components/toc.blade.php
@@ -10,11 +10,11 @@
                     <li>
                         <a
                             href="#{{ $item['id'] }}"
-                            class="inline-flex items-start gap-1 leading-tight text-indigo-900/75 dark:text-indigo-400"
+                            class="inline-flex items-center gap-1 leading-tight text-indigo-900/75 dark:text-indigo-400"
                             @if ($item['level'] > 2) style="margin-left: calc(1rem * {{ $item['level'] - 2 }})" @endif
                             @click="window.fathom?.trackGoal('2WUWXET3', 0)"
                         >
-                            <x-heroicon-o-hashtag class="flex-shrink-0 w-3 h-3 text-indigo-900/40 dark:text-indigo-400/50 translate-y-[1px]" />
+                            <x-heroicon-o-hashtag class="flex-shrink-0 w-3 h-3 text-indigo-900/40 dark:text-indigo-400/50" />
                             {{ $item['title'] }}
                         </a>
                     </li>
