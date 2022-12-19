387939f19533ee943bd71bb45037ceacbe910fb4
Benjamin Crozat <benjamincrozat@me.com>
2022-12-19 22:15:27 +0100

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,11 @@ import Intersect from '@alpinejs/intersect'
 
 Alpine.plugin(Intersect)
 Alpine.start()
+
+if (typeof ezConsentCategories == 'object' && typeof __ezconsent == 'object') {
+    window.ezConsentCategories.preferences = true;
+    window.ezConsentCategories.statistics = true;
+    window.ezConsentCategories.marketing = true;
+
+    __ezconsent.setEzoicConsentSettings(window.ezConsentCategories);
+}
