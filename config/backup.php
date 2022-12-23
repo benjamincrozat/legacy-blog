11e14676c7f2fe2f20849a61c25e0e1f0f04228c
Benjamin Crozat <benjamincrozat@me.com>
2022-12-23 09:15:14 +0100

    WIP

--- config/backup.php
+++ config/backup.php
@@ -6,7 +6,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'laravel-backup'),
+        'name' => preg_replace('/https?:\/\//', '', env('APP_NAME', 'laravel-backup')),
 
         'source' => [
             'files' => [
@@ -202,7 +202,7 @@ return [
      */
     'monitor_backups' => [
         [
-            'name' => env('APP_NAME', 'laravel-backup'),
+            'name' => preg_replace('/https?:\/\//', '', env('APP_NAME', 'laravel-backup')),
             'disks' => ['local'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
