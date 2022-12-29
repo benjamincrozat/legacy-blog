fbd6408583b0abc5ddd6c7289d638fda643d2899
Benjamin Crozat <benjamincrozat@me.com>
2022-12-29 14:24:23 +0100

    Update pinned-post.blade.php

--- resources/views/components/pinned-post.blade.php
+++ resources/views/components/pinned-post.blade.php
@@ -7,7 +7,7 @@
             class="aspect-video object-cover w-full"
         />
 
-        <figcaption class="absolute bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-black/50 flex items-center justify-between gap-4 font-bold px-4 py-3 text-white" style="text-shadow: 1px 1px 1px rgba(0, 0, 0, .5)">
+        <figcaption class="absolute bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-black/50 flex items-center justify-between gap-4 font-bold px-4 py-3 text-white" style="text-shadow: 1px 1px 1px rgba(0, 0, 0, .3)">
             <span class="truncate">{{ $post->title }}</span>
             <x-heroicon-o-arrow-right class="flex-shrink-0 opacity-85 w-3 h-3" />
         </figcaption>
