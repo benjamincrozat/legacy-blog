ef9dda75c3a8c1a401f523a5466161151c5a4d6a
Benjamin Crozat <benjamincrozat@me.com>
2023-01-06 19:05:00 +0100

    WIP

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,15 +1,67 @@
-<div {{ $attributes->merge(['class' => 'flex flex-wrap justify-between gap-8']) }}>
+<div {{ $attributes->merge(['class' => 'flex flex-wrap items-center justify-between gap-8']) }}>
     <a href="{{ route('home') }}">
         <x-icon-logo class="h-8 md:h-9" />
     </a>
 
-    <nav class="flex items-center gap-6 sm:gap-8">
-        <a
-            href="{{ route('consulting.laravel') }}"
-            class="bg-gradient-to-r from-indigo-300 dark:from-indigo-500 to-indigo-400 dark:to-indigo-600 hover:-hue-rotate-90 duration-500 transition-all font-semibold px-4 sm:px-6 py-2 rounded shadow-lg text-sm text-white w-full"
-            @click="window.fathom?.trackGoal('SJKONFQC', 0)"
+    <nav class="flex items-center gap-6 sm:gap-8 font-semibold relative text-white" x-data="{ open: false }" @click.away="open = false">
+        <button
+            class="flex items-center gap-2 pl-4 pr-3 py-2 text-sm w-full"
+            :class="{
+                'bg-gradient-to-r from-indigo-300 dark:from-indigo-500 to-indigo-400 dark:to-indigo-600 hover:-hue-rotate-90 rounded shadow-lg': ! open,
+                'bg-gray-800 rounded-t': open,
+            }"
+            @click="open = ! open"
         >
-            Book me
-        </a>
+            Products
+            <x-heroicon-o-chevron-down class="w-3 h-3 translate-y-px transition-transform duration-500" x-bind:class="{ 'rotate-180': open }" />
+        </button>
+
+        <ul
+            class="absolute top-full right-0 hidden bg-gray-800 min-w-[320px] py-2 rounded rounded-tr-none shadow-xl text-sm"
+            x-bind:class="{ '!block': open }"
+            x-transition
+        >
+            <li class="font-light text-gray-400 my-2 px-4 text-xs uppercase">
+                Courses
+            </li>
+
+            <li>
+                <a
+                    href="#"
+                    class="hover:bg-blue-400 flex items-center gap-1 group px-4 py-2 hover:text-white transition-colors"
+                    @click="window.fathom?.trackGoal('SJKONFQC', 0)"
+                >
+                    SEO Wizardry for Developers
+                    <span
+                        class="bg-gradient-to-r from-indigo-300 to-indigo-500 group-hover:-hue-rotate-90 leading-none px-3 py-2 rounded-full scale-75 text-xs translate-y-px transition-colors"
+                    >
+                        FREE
+                    </span>
+                </a>
+            </li>
+
+            <li class="font-light text-gray-400 my-2 px-4 text-xs uppercase">
+                Consulting
+            </li>
+
+            <li>
+                <a
+                    href="{{ route('consulting.laravel') }}"
+                    class="hover:bg-blue-400 block px-4 py-2 hover:text-white transition-colors"
+                    @click="window.fathom?.trackGoal('SJKONFQC', 0)"
+                >
+                    Laravel
+                </a>
+            </li>
+
+            <li>
+                <a
+                    href="#"
+                    class="hover:bg-blue-400 block px-4 py-2 hover:text-white transition-colors"
+                >
+                    SEO
+                </a>
+            </li>
+        </ul>
     </nav>
 </div>
