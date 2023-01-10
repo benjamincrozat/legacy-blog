477569b3f3dc46cfcf88081f0cc5f4dc50e5beff
Benjamin Crozat <benjamincrozat@me.com>
2023-01-10 07:03:19 +0100

    WIP

--- resources/views/components/toc.blade.php
+++ resources/views/components/toc.blade.php
@@ -1,25 +1,23 @@
 @if ($toc->isNotEmpty() && $toc->count() > 1)
     <div {{ $attributes->except('toc') }}>
-        <div class="font-semibold mb-2">
+        <p class="font-semibold mb-2">
             Table of contents
-        </div>
+        </p>
 
-        <nav class="mt-3">
-            <ul class="grid gap-2">
-                @foreach ($toc as $item)
-                    <li>
-                        <a
-                            href="#{{ $item['id'] }}"
-                            class="inline-flex items-center gap-1 leading-tight text-indigo-900/75 dark:text-indigo-400"
-                            @if ($item['level'] > 2) style="margin-left: calc(1rem * {{ $item['level'] - 2 }})" @endif
-                            @click="window.fathom?.trackGoal('2WUWXET3', 0)"
-                        >
-                            <x-heroicon-o-hashtag class="flex-shrink-0 w-3 h-3 text-indigo-900/40 dark:text-indigo-400/50" />
-                            {{ $item['title'] }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </nav>
+        <ul class="grid gap-2 mt-3">
+            @foreach ($toc as $item)
+                <li>
+                    <a
+                        href="#{{ $item['id'] }}"
+                        class="inline-flex items-center gap-1 leading-tight text-indigo-900/75 dark:text-indigo-400"
+                        @if ($item['level'] > 2) style="margin-left: calc(1rem * {{ $item['level'] - 2 }})" @endif
+                        @click="window.fathom?.trackGoal('2WUWXET3', 0)"
+                    >
+                        <x-heroicon-o-hashtag class="flex-shrink-0 w-3 h-3 text-indigo-900/40 dark:text-indigo-400/50" />
+                        {{ $item['title'] }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
     </div>
 @endif
