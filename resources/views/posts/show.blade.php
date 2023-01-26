fb1baaa49ee72c7721b02a865e3f5c3be71cfce7
Benjamin Crozat <benjamincrozat@me.com>
2023-01-26 09:23:41 +0100

    WIP

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,10 +4,6 @@
     :image="$post->image"
     class="dark:bg-gray-900 text-gray-600 dark:text-gray-300"
 >
-    @push('head')
-        <script defer src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    @endpush
-
     <x-posts::nav
         :funnel="$post->promotes_affiliate_links"
         class="container mt-6"
