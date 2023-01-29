69ea68b4b849e592a2f246111e3df6bfb16e2e90
Benjamin Crozat <benjamincrozat@me.com>
2023-01-29 15:50:59 +0100

    WIP

--- resources/views/consulting/laravel.blade.php
+++ resources/views/consulting/laravel.blade.php
@@ -2,59 +2,61 @@
     title="Laravel developer for hire"
     description="Hire experienced & dedicated Laravel developers, programmers, experts and freelancers for top-notch web app development. Flexible hiring options available."
 >
-    <h1>Hire a Laravel developer</h1>
+    <div class="!container content py-8">
+        <h1>Hire a Laravel developer</h1>
 
-    <p>Looking to hire a Laravel developer for your next project? Look no further. Our team of experienced Laravel developers are ready to take on your project and deliver top-notch results.</p>
+        <p>Looking to hire a Laravel developer for your next project? Look no further. Our team of experienced Laravel developers are ready to take on your project and deliver top-notch results.</p>
 
-    <h2>Laravel Developers for Hire</h2>
+        <h2>Laravel Developers for Hire</h2>
 
-    Our team of Laravel developers have a wealth of experience in developing high-quality web applications using the Laravel framework. Whether you need a simple website or a complex web application, our developers have the skills and expertise to deliver.</p>
+        Our team of Laravel developers have a wealth of experience in developing high-quality web applications using the Laravel framework. Whether you need a simple website or a complex web application, our developers have the skills and expertise to deliver.</p>
 
-    <h2>Hiring Laravel Developer</h2>
+        <h2>Hiring Laravel Developer</h2>
 
-    When it comes to hiring a Laravel developer, you want someone who is not only experienced but also dedicated to delivering top-quality work. Our developers are not only experts in the Laravel framework but also have a passion for delivering top-notch results.</p>
+        When it comes to hiring a Laravel developer, you want someone who is not only experienced but also dedicated to delivering top-quality work. Our developers are not only experts in the Laravel framework but also have a passion for delivering top-notch results.</p>
 
-    <h2>Laravel Developer for Hire</h2>
+        <h2>Laravel Developer for Hire</h2>
 
-    If you're looking for a Laravel developer for hire, our team is here to help. Our developers are experts in the Laravel framework and have a proven track record of delivering high-quality web applications.</p>
+        If you're looking for a Laravel developer for hire, our team is here to help. Our developers are experts in the Laravel framework and have a proven track record of delivering high-quality web applications.</p>
 
-    <h2>Hire Laravel Developer</h2>
+        <h2>Hire Laravel Developer</h2>
 
-    Hiring a Laravel developer can be a daunting task, but with our team, it's easy. Our developers are experts in the Laravel framework and have a wealth of experience in developing high-quality web applications.</p>
+        Hiring a Laravel developer can be a daunting task, but with our team, it's easy. Our developers are experts in the Laravel framework and have a wealth of experience in developing high-quality web applications.</p>
 
-    <h2>Hire Laravel Developers</h2>
+        <h2>Hire Laravel Developers</h2>
 
-    If you need more than one Laravel developer for your project, our team can help. We have a pool of experienced Laravel developers who are ready to take on your project and deliver top-notch results.</p>
+        If you need more than one Laravel developer for your project, our team can help. We have a pool of experienced Laravel developers who are ready to take on your project and deliver top-notch results.</p>
 
-    <h2>Hire Dedicated Laravel Developer</h2>
+        <h2>Hire Dedicated Laravel Developer</h2>
 
-    If you're looking for a dedicated Laravel developer for your project, our team has you covered. Our developers are not only experts in the Laravel framework but also dedicated to delivering top-quality work.</p>
+        If you're looking for a dedicated Laravel developer for your project, our team has you covered. Our developers are not only experts in the Laravel framework but also dedicated to delivering top-quality work.</p>
 
-    <h2>Hire Dedicated Laravel Developers</h2>
+        <h2>Hire Dedicated Laravel Developers</h2>
 
-    If you need a team of dedicated Laravel developers for your project, our team can help. We have a pool of experienced and dedicated Laravel developers who are ready to take on your project and deliver top-notch results.</p>
+        If you need a team of dedicated Laravel developers for your project, our team can help. We have a pool of experienced and dedicated Laravel developers who are ready to take on your project and deliver top-notch results.</p>
 
-    <h2>Hire Laravel Experts</h2>
+        <h2>Hire Laravel Experts</h2>
 
-    When it comes to hiring a Laravel expert, our team has you covered. Our developers are not only experts in the Laravel framework but also have a wealth of experience in developing high-quality web applications.</p>
+        When it comes to hiring a Laravel expert, our team has you covered. Our developers are not only experts in the Laravel framework but also have a wealth of experience in developing high-quality web applications.</p>
 
-    <h2>Hire Laravel Expert</h2>
+        <h2>Hire Laravel Expert</h2>
 
-    If you're looking for a single Laravel expert for your project, our team has you covered. Our developers are not only experts in the Laravel framework but also have a proven track record of delivering high-quality web applications.</p>
+        If you're looking for a single Laravel expert for your project, our team has you covered. Our developers are not only experts in the Laravel framework but also have a proven track record of delivering high-quality web applications.</p>
 
-    <h2>Laravel Programmer</h2>
+        <h2>Laravel Programmer</h2>
 
-    Our team of Laravel programmers are experts in the Laravel framework and have a wealth of experience in developing high-quality web applications. Whether you need a simple website or a complex web application, our programmers have the skills and expertise to deliver.</p>
+        Our team of Laravel programmers are experts in the Laravel framework and have a wealth of experience in developing high-quality web applications. Whether you need a simple website or a complex web application, our programmers have the skills and expertise to deliver.</p>
 
-    <h2>Hire Laravel Programmers</h2>
+        <h2>Hire Laravel Programmers</h2>
 
-    If you need more than one Laravel programmer for your project, our team can help. We have a pool of experienced Laravel programmers who are ready to take on your project and deliver top-notch results.</p>
+        If you need more than one Laravel programmer for your project, our team can help. We have a pool of experienced Laravel programmers who are ready to take on your project and deliver top-notch results.</p>
 
-    <h2>Hire Laravel Programmer</h2>
+        <h2>Hire Laravel Programmer</h2>
 
-    If you're looking for a single Laravel programmer for your project, our team has you covered. Our programmers are experts in the Laravel framework and have a proven track record of delivering high-quality web applications.</p>
+        If you're looking for a single Laravel programmer for your project, our team has you covered. Our programmers are experts in the Laravel framework and have a proven track record of delivering high-quality web applications.</p>
 
-    <h2>Laravel Programmers</h2>
+        <h2>Laravel Programmers</h2>
 
-    Our team of Laravel programmers are experts in the Laravel framework and have a wealth of experience in developing high-quality web applications. Whether you need a simple website or a complex web application, our programmers have the skills and expertise to deliver.</p>
+        Our team of Laravel programmers are experts in the Laravel framework and have a wealth of experience in developing high-quality web applications. Whether you need a simple website or a complex web application, our programmers have the skills and expertise to deliver.</p>
+    </div>
 </x-app>
