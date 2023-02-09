890194165c92bd99e2a5651e81d9483c98913399
Benjamin Crozat <benjamincrozat@me.com>
2023-02-09 11:47:19 +0100

    WIP

--- tests/Feature/App/Http/Controllers/HomeControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomeControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\App\Http\Controllers;
 use Tests\TestCase;
 use App\Models\Post;
 use Illuminate\Support\Collection;
+use Illuminate\Pagination\Paginator;
 
 class HomeControllerTest extends TestCase
 {
@@ -21,6 +22,19 @@ class HomeControllerTest extends TestCase
         $this->assertCount(4, $response->viewData('pins'));
     }
 
+    public function test_it_lists_popular_posts() : void
+    {
+        Post::factory(10)->create();
+
+        $response = $this
+            ->get(route('home'))
+            ->assertOk()
+            ->assertViewIs('home');
+
+        $this->assertInstanceOf(Collection::class, $response->viewData('popular'));
+        $this->assertCount(6, $response->viewData('popular'));
+    }
+
     public function test_it_lists_posts() : void
     {
         Post::factory(10)->create();
@@ -30,7 +44,7 @@ class HomeControllerTest extends TestCase
             ->assertOk()
             ->assertViewIs('home');
 
-        $this->assertInstanceOf(Collection::class, $response->viewData('posts'));
+        $this->assertInstanceOf(Paginator::class, $response->viewData('posts'));
         $this->assertCount(10, $response->viewData('posts'));
     }
 }
