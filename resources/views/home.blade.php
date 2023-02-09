531008c38e4baf5815d6f1054dccd4bf27581510
Benjamin Crozat <benjamincrozat@me.com>
2023-02-09 11:58:21 +0100

    WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,62 +5,66 @@
 >
     <x-nav class="container mt-6" />
 
-    <x-newsletter class="container mt-16">
-        <div class="md:flex md:items-center mt-8">
-            <img loading="lazy" src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}?s=256" width="96" height="96" alt="Benjamin Crozat" class="float-right md:float-none mb-4 ml-4 mt-4 md:m-0 md:order-1 rounded-full w-[80px] h-[80px] md:w-[96px] md:h-[96px]" />
+    @if ($posts->onFirstPage())
+        <x-newsletter class="container mt-16">
+            <div class="md:flex md:items-center mt-8">
+                <img loading="lazy" src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}?s=256" width="96" height="96" alt="Benjamin Crozat" class="float-right md:float-none mb-4 ml-4 mt-4 md:m-0 md:order-1 rounded-full w-[80px] h-[80px] md:w-[96px] md:h-[96px]" />
 
-            <div>
-                <p>
-                    Hi there! 👋
-                </p>
+                <div>
+                    <p>
+                        Hi there! 👋
+                    </p>
 
-                <p class="mt-4">
-                    <strong class="font-semibold">I'm Benjamin Crozat</strong> and I have <strong class="font-semibold">10+ years of expertise</strong> in web development.
-                </p>
+                    <p class="mt-4">
+                        <strong class="font-semibold">I'm Benjamin Crozat</strong> and I have <strong class="font-semibold">10+ years of expertise</strong> in web development.
+                    </p>
 
-                <p class="mt-4">
-                    Understanding and building for the web can give you a significant advantage. It provides job security and allows you to start a business from scratch.
-                </p>
+                    <p class="mt-4">
+                        Understanding and building for the web can give you a significant advantage. It provides job security and allows you to start a business from scratch.
+                    </p>
 
-                <p class="mt-4">
-                    I want to share with you everything I learn, <strong class="font-semibold">for free</strong>.
-                </p>
+                    <p class="mt-4">
+                        I want to share with you everything I learn, <strong class="font-semibold">for free</strong>.
+                    </p>
+                </div>
             </div>
-        </div>
-    </x-newsletter>
+        </x-newsletter>
 
-    @if ($pins->isNotEmpty())
-        <section class="md:container md:max-w-[1024px] mt-16">
-            <div class="font-semibold px-4 md:px-0 text-center text-xl">
-                Pinned articles
+        @if ($pins->isNotEmpty())
+            <section class="md:container md:max-w-[1024px] mt-16">
+                <div class="font-semibold px-4 md:px-0 text-center text-xl">
+                    Pinned articles
+                </div>
+
+                <div class="flex md:grid md:grid-cols-2 gap-2 mt-8 px-4 md:px-0 overflow-x-scroll md:overflow-x-visible snap-x md:snap-none snap-mandatory">
+                    @foreach ($pins as $pin)
+                        <x-pinned-post :post="$pin->post" :first="$loop->first" />
+                    @endforeach
+                </div>
+            </section>
+        @endif
+
+        <section class="container max-w-[1024px] mt-16">
+            <div class="font-semibold px-4 sm:px-0 text-center text-xl">
+                Most popular last week
             </div>
 
-            <div class="flex md:grid md:grid-cols-2 gap-2 mt-8 px-4 md:px-0 overflow-x-scroll md:overflow-x-visible snap-x md:snap-none snap-mandatory">
-                @foreach ($pins as $pin)
-                    <x-pinned-post :post="$pin->post" :first="$loop->first" />
-                @endforeach
-            </div>
+            @if ($popular->isNotEmpty())
+                <div class="grid md:grid-cols-2 gap-4 mt-8">
+                    @foreach ($popular as $post)
+                        <x-post :post="$post" @click="window.fathom?.trackGoal('HH0P1ACM', 0)" />
+                    @endforeach
+                </div>
+            @endif
         </section>
     @endif
 
     <section class="container max-w-[1024px] mt-16">
-        <div class="font-semibold px-4 sm:px-0 text-center text-xl">
-            Most popular last week
-        </div>
-
-        @if ($popular->isNotEmpty())
-            <div class="grid md:grid-cols-2 gap-4 mt-8">
-                @foreach ($popular as $post)
-                    <x-post :post="$post" @click="window.fathom?.trackGoal('HH0P1ACM', 0)" />
-                @endforeach
+        @if ($posts->onFirstPage())
+            <div class="font-semibold px-4 sm:px-0 text-center text-xl">
+                Latest
             </div>
         @endif
-    </section>
-
-    <section class="container max-w-[1024px] mt-16">
-        <div class="font-semibold px-4 sm:px-0 text-center text-xl">
-            Latest
-        </div>
 
         @if ($posts->isNotEmpty())
             <div class="grid md:grid-cols-2 gap-4 mt-8">
@@ -77,12 +81,14 @@
         @endif
     </section>
 
-    <x-newsletter class="container mt-16">
-        <x-slot:headline>
-            Want more content?<br />
-            Subscribe to my <span class="bg-clip-text bg-gradient-to-r from-indigo-300 to-indigo-400"><span class="text-transparent">free</span></span> newsletter!
-        </x-slot:headline>
-    </x-newsletter>
+    @if ($posts->onFirstPage())
+        <x-newsletter class="container mt-16">
+            <x-slot:headline>
+                Want more content?<br />
+                Subscribe to my <span class="bg-clip-text bg-gradient-to-r from-indigo-300 to-indigo-400"><span class="text-transparent">free</span></span> newsletter!
+            </x-slot:headline>
+        </x-newsletter>
+    @endif
 
     <div class="bg-gray-900 dark:bg-black mt-16">
         <x-footer class="text-gray-200" />
