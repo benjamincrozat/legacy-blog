ab1141ec31524fcd7bbcb4a7be8695c9d5d9f1eb
Benjamin Crozat <benjamincrozat@me.com>
2023-02-15 08:48:46 +0100

    UI fix

--- resources/views/posts/components/breadcrumb-item.blade.php
+++ resources/views/posts/components/breadcrumb-item.blade.php
@@ -4,13 +4,13 @@
     @if ($attributes->get('href'))
         <a
             href="{{ $href }}"
-            class="text-indigo-400"
+            class="line-clamp-1 text-indigo-400"
             @click="window.fathom?.trackGoal('I4R3OAAU', 0)"
         >
             {{ $slot }}
         </a>
     @else
-        <span class="text-gray-400">
+        <span class="line-clamp-1 text-gray-400">
             {{ $slot }}
         </span>
     @endif
