02e44aadb3cd20aae7aaa9cdc6fd254bacb880f6
Benjamin Crozat <benjamincrozat@me.com>
2023-02-15 12:56:49 +0100

    WIP

--- resources/views/components/pinned-post.blade.php
+++ resources/views/components/pinned-post.blade.php
@@ -1,16 +1,27 @@
-<a href="{{ route('posts.show', $post->slug) }}" @click="window.fathom?.trackGoal('OKJIR46O', 0)" class="block flex-shrink-0 overflow-hidden relative snap-start sm:snap-center md:snap-normal scroll-ml-4 md:scroll-ml-0 w-[90%] sm:w-[70%] md:w-auto">
-    <figure {{ $attributes->except('post') }}>
+<div {{ $attributes->except('first', 'post')->merge(['class' => 'block flex-shrink-0 overflow-hidden relative snap-start sm:snap-center md:snap-normal scroll-ml-4 md:scroll-ml-0 w-[90%] sm:w-[70%] md:w-auto']) }}>
+    <a href="{{ route('posts.show', $post->slug) }}" @click="window.fathom?.trackGoal('OKJIR46O', 0)">
         <img
             @if (empty($first)) loading="lazy" @endif
             src="{{ str_replace('w_auto', 'w_600', $post->image) }}"
             alt="{{ $post->title }}"
             class="aspect-video object-cover rounded-md w-full"
         />
+    </a>
 
-        <figcaption class="inline-block py-4">
-            <p class="font-semibold">{{ $post->title }}</p>
+    <div class="font-semibold mt-3">{{ $post->title }}</div>
 
-            <p class="leading-relaxed mt-3 text-sm">{{ $post->description }}</p>
-        </figcaption>
-    </figure>
-</a>
+    <div class="flex items-center gap-2 mt-2 text-sm">
+        <img
+            loading="lazy"
+            src="https://www.gravatar.com/avatar/{{ md5($post->user->email) }}"
+            width="18"
+            height="18"
+            alt="{{ $post->user->name }}"
+            class="-translate-y-[.5px] rounded-full"
+        />
+
+        <div>
+            <a href="{{ route('consulting.cto') }}" target="_blank" rel="noopener noreferrer" class="font-medium" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">{{ $post->user->name }}</a> — <span class="opacity-75">@choice(':count&nbsp;min|:count&nbsp;mins', $post->read_time) read</span>
+        </div>
+    </div>
+</div>
