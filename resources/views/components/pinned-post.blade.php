fd2fbd14f297a42236488a2459fa481085eb6580
Benjamin Crozat <benjamincrozat@me.com>
2023-02-16 08:44:37 +0100

    WIP

--- resources/views/components/pinned-post.blade.php
+++ resources/views/components/pinned-post.blade.php
@@ -1,5 +1,8 @@
 <div {{ $attributes->except('first', 'post')->merge(['class' => 'block flex-shrink-0 overflow-hidden relative snap-start sm:snap-center md:snap-normal scroll-ml-4 md:scroll-ml-0 w-[90%] sm:w-[70%] md:w-auto']) }}>
-    <a href="{{ route('posts.show', $post->slug) }}" @click="window.fathom?.trackGoal('OKJIR46O', 0)">
+    <a
+        href="{{ route('posts.show', $post->slug) }}"
+        @click="window.fathom?.trackGoal('OKJIR46O', 0)"
+    >
         <img
             @if (empty($first)) loading="lazy" @endif
             src="{{ str_replace('w_auto', 'w_600', $post->image) }}"
@@ -8,7 +11,13 @@
         />
     </a>
 
-    <a href="{{ route('posts.show', $post->slug) }}" class="font-semibold inline-block mt-3">{{ $post->title }}</a>
+    <a
+        href="{{ route('posts.show', $post->slug) }}"
+        class="font-semibold inline-block mt-4"
+        @click="window.fathom?.trackGoal('OKJIR46O', 0)"
+    >
+        {{ $post->title }}
+    </a>
 
     <div class="flex items-center gap-2 mt-2 text-sm">
         <img
@@ -21,7 +30,19 @@
         />
 
         <div>
-            <a href="{{ route('consulting.cto') }}" target="_blank" rel="noopener noreferrer" class="font-medium" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">{{ $post->user->name }}</a> — <span class="opacity-75">@choice(':count&nbsp;min|:count&nbsp;mins', $post->read_time) read</span>
+            <a
+                href="{{ route('consulting.cto') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="font-medium"
+                @click="window.fathom?.trackGoal('LNRXVF3B', 0)"
+            >
+                {{ $post->user->name }}
+            </a>
+            —
+            <span class="opacity-75">
+                @choice(':count&nbsp;min|:count&nbsp;mins', $post->read_time) read
+            </span>
         </div>
     </div>
 </div>
