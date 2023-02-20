9ce2fe3952d7c2a7a528f3b120d596c8f672dc71
Benjamin Crozat <benjamincrozat@me.com>
2023-02-20 17:53:11 +0100

    WIP

--- tests/Feature/App/Support/TreeGeneratorTest.php
+++ tests/Feature/App/Support/TreeGeneratorTest.php
@@ -3,11 +3,34 @@
 namespace Tests\Feature\App\Support;
 
 use Tests\TestCase;
+use App\Support\TreeGenerator;
 
 class TreeGeneratorTest extends TestCase
 {
     public function test_it_works() : void
     {
-        $this->markTestSkipped();
+        $tree = TreeGenerator::generate(<<<HTML
+<h1 id="foo">Foo</h1>
+<h2 id="bar">Bar</h2>
+<h3 id="baz">Baz</h3>
+HTML);
+
+        $this->assertEquals([
+            [
+                'id' => 'foo',
+                'title' => 'Foo',
+                'level' => 1,
+            ],
+            [
+                'id' => 'bar',
+                'title' => 'Bar',
+                'level' => 2,
+            ],
+            [
+                'id' => 'baz',
+                'title' => 'Baz',
+                'level' => 3,
+            ],
+        ], $tree);
     }
 }
