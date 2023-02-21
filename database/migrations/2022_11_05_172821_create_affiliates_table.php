5b8ff94e76442fbd7d2505fa9c787cb508af66a8
Benjamin Crozat <benjamincrozat@me.com>
2023-02-21 18:36:21 +0100

    WIP

--- database/migrations/2022_11_05_172821_create_affiliates_table.php
+++ database/migrations/2022_11_05_172821_create_affiliates_table.php
@@ -9,10 +9,23 @@ return new class() extends Migration {
     {
         Schema::create('affiliates', function (Blueprint $table) {
             $table->id();
-            $table->string('image')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('screenshot')->nullable();
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('link');
+            $table->text('take')->nullable();
+            $table->unsignedInteger('rating')->default(0);
+            $table->string('annual_discount')->nullable();
+            $table->string('guarantee')->nullable();
+            $table->text('content')->nullable();
+            $table->text('pricing')->nullable();
+            $table->text('key_features')->nullable();
+            $table->text('pros')->nullable();
+            $table->text('cons')->nullable();
+            $table->string('highlight_title')->nullable();
+            $table->text('highlight_text')->nullable();
+            $table->unsignedInteger('position')->nullable();
             $table->timestamps();
         });
     }
