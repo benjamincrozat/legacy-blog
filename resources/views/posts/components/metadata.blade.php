a6e4f13fa688846f345afba5f4797e65bb30be74
Benjamin Crozat <benjamincrozat@me.com>
2023-02-21 18:15:58 +0100

    WIP

--- resources/views/posts/components/metadata.blade.php
+++ resources/views/posts/components/metadata.blade.php
@@ -1,4 +1,4 @@
 <div {{ $attributes->merge(['class' => 'text-sm']) }}>
-    Published by <a href="{{ route('consulting.cto') }}" class="font-normal underline" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">{{ $name }}</a>
+    <a href="{{ route('consulting.cto') }}" class="font-normal underline" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">{{ $name }}</a>
     — Updated on <time datetime="{{ $updatedAt->toDateTimeString() }}">{{ $updatedAt->isoFormat('ll') }}</time>
 </div>
