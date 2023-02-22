49ea88752b2457b76048c99fc754dc190d67607a
Benjamin Crozat <benjamincrozat@me.com>
2023-02-22 15:51:24 +0100

    WIP

--- resources/views/posts/components/author.blade.php
+++ resources/views/posts/components/author.blade.php
@@ -9,5 +9,5 @@
         </div>
     </div>
 
-    <img src="https://www.gravatar.com/avatar/{{ md5($email) }}?s=192" width="96" height="96" alt="{{ $name }}" class="-translate-y-[.5px] flex-shrink-0 w-16 h-16 sm:w-24 sm:h-24 order-1 sm:order-none rounded-full" />
+    <img src="https://www.gravatar.com/avatar/{{ md5($email) }}?s=192" width="96" height="96" alt="{{ $name }}" class="sm:-translate-y-2 flex-shrink-0 w-16 h-16 sm:w-24 sm:h-24 order-1 sm:order-none rounded-full" />
 </aside>
