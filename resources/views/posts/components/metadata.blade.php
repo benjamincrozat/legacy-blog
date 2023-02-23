b5b1d929db2360558c72fbfd6b187b691ad202b9
Benjamin Crozat <benjamincrozat@me.com>
2023-02-23 10:03:31 +0100

    WIP

--- resources/views/posts/components/metadata.blade.php
+++ resources/views/posts/components/metadata.blade.php
@@ -1,4 +1,4 @@
 <div {{ $attributes->merge(['class' => 'text-sm']) }}>
-    <a href="{{ route('consulting.cto') }}" class="font-normal underline" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">{{ $name }}</a>
+    <a href="https://twitter.com/benjamincrozat" class="font-normal underline" @click="window.fathom?.trackGoal('LNRXVF3B', 0)">{{ $name }}</a>
     — Updated on <time datetime="{{ $modifiedAt?->toDateTimeString() }}">{{ $modifiedAt?->isoFormat('ll') }}</time>
 </div>
