84e04d54ee4cb576d6c6ede7ce45e9a91d7f34ec
Benjamin Crozat <benjamincrozat@me.com>
2023-02-23 22:20:53 +0100

    WIP

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -3,14 +3,14 @@
         'rel' => 'nofollow noopener noreferrer',
         'class' => 'block container md:max-w-screen-sm'
     ]) }}>
-        <div class="border flex gap-4 p-4 rounded-md text-sm">
+        <div class="border sm:flex sm:items-center sm:gap-4 p-4 rounded-md text-sm">
             <img
                 src="{{ $affiliate->icon }}"
                 alt="{{ $affiliate->name }}"
                 class="aspect-square flex-shrink-0 rounded w-[48px] sm:w-[64px] h-[48px] sm:h-[64px]"
             />
 
-            <div>
+            <div class="mt-4 sm:mt-0">
                 <div class="font-bold">{{ $affiliate->ad_title }}</div>
                 <div>{!! $affiliate->rendered_ad_content !!}</div>
             </div>
