7486d8c7677fecc8a184091c9f818d1e19331de3
Benjamin Crozat <benjamincrozat@me.com>
2023-02-23 22:25:09 +0100

    WIP

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -3,14 +3,14 @@
         'rel' => 'nofollow noopener noreferrer',
         'class' => 'block container md:max-w-screen-sm'
     ]) }}>
-        <div class="border sm:flex sm:items-center sm:gap-4 p-4 rounded-md text-sm">
+        <div class="border flex flex-wrap sm:flex-nowrap items-center justify-center sm:justify-start sm:gap-4 p-4 rounded-md text-center sm:text-left text-sm">
             <img
                 src="{{ $affiliate->icon }}"
                 alt="{{ $affiliate->name }}"
-                class="aspect-square flex-shrink-0 rounded w-[48px] sm:w-[64px] h-[48px] sm:h-[64px]"
+                class="aspect-square flex-shrink-0 inline rounded w-[48px] sm:w-[64px] h-[48px] sm:h-[64px]"
             />
 
-            <div class="mt-4 sm:mt-0">
+            <div class="mt-4 sm:mt-0 w-full">
                 <div class="font-bold">{{ $affiliate->ad_title }}</div>
                 <div>{!! $affiliate->rendered_ad_content !!}</div>
             </div>
