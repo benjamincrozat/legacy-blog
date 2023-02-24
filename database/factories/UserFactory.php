2eab42c7a5669c0cd6fb0c57f7a90be57e23f922
Benjamin Crozat <benjamincrozat@me.com>
2023-02-24 11:31:53 +0100

    WIP

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'description' => collect([fake()->paragraph(), null])->random(),
-            'twitter' => fake()->url(),
+            'twitter_url' => fake()->url(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
