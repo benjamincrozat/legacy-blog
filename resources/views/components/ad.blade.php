929343cb8b267ffdda19ecbf992e275e5bb46d40
Benjamin Crozat <benjamincrozat@me.com>
2023-02-24 13:40:17 +0100

    WIP

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -1,32 +1,34 @@
 @if ($ads->isNotEmpty() && $affiliate = $ads->random())
     <div {{ $attributes->merge(['class' => 'container md:max-w-screen-sm']) }}>
-        <div class="border border-gray-200 dark:border-gray-800 flex flex-wrap sm:flex-nowrap items-center justify-center sm:justify-start sm:gap-4 p-4 rounded-md text-center sm:text-left text-sm">
+        <div class="border border-gray-200 dark:border-gray-800 flex sm:items-center justify-between sm:justify-start gap-4 p-4 rounded-md text-sm">
             <a
                 href="{{ route('affiliate', $affiliate) }}"
                 rel="nofollow noopener noreferral"
-                class="flex-shrink-0"
+                class="flex-shrink-0 order-2 sm:order-none"
                 @click="window.fathom?.trackGoal('ODF3S05G', 0)"
             >
                 <img
                     src="{{ $affiliate->icon }}"
                     alt="{{ $affiliate->name }}"
-                    class="aspect-square rounded w-[48px] h-[48px]"
+                    class="aspect-square rounded w-[64px] h-[64px]"
                 />
             </a>
 
-            <div class="leading-tight mt-4 sm:mt-0 w-full">
-                <div class="font-bold">{{ $affiliate->ad_title }}</div>
-                <div class="mt-1">{!! $affiliate->rendered_ad_content !!}</div>
-            </div>
+            <div class="sm:flex sm:items-center sm:justify-between sm:gap-4 order-1 sm:order-none">
+                <div class="leading-tight w-full">
+                    <div class="font-bold">{{ $affiliate->ad_title }}</div>
+                    <div class="mt-1">{!! $affiliate->rendered_ad_content !!}</div>
+                </div>
 
-            <a
-                href="{{ route('affiliate', $affiliate) }}"
-                class="bg-gradient-to-r from-emerald-400 dark:from-emerald-700 to-emerald-500 dark:to-emerald-800 hover:hue-rotate-90 inline-block !border-0 flex-shrink-0 font-semibold leading-tight mt-4 sm:mt-0 sm:mx-auto px-3 py-2 rounded shadow-md sm:table text-center !text-emerald-50 duration-500 transition-[filter]"
-                rel="nofollow noopener noreferral"
-                @click="window.fathom?.trackGoal('ODF3S05G', 0)"
-            >
-                Go to site
-            </a>
+                <a
+                    href="{{ route('affiliate', $affiliate) }}"
+                    class="bg-gradient-to-r from-emerald-400 dark:from-emerald-700 to-emerald-500 dark:to-emerald-800 hover:hue-rotate-90 inline-block !border-0 flex-shrink-0 font-semibold leading-tight mt-4 sm:mt-0 px-3 py-2 rounded shadow-md !text-emerald-50 duration-500 transition-[filter]"
+                    rel="nofollow noopener noreferral"
+                    @click="window.fathom?.trackGoal('ODF3S05G', 0)"
+                >
+                    Go to site
+                </a>
+            </div>
         </div>
     </div>
 @endif
