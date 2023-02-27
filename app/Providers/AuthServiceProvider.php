d60ad7d5d84f06e7be30cd35884a704a6f05e100
Benjamin Crozat <benjamincrozat@me.com>
2023-02-27 09:50:42 +0100

    Mirrored laravel/laravel + updated deps

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -11,9 +11,4 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [];
-
-    public function boot() : void
-    {
-        $this->registerPolicies();
-    }
 }
