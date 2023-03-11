956ccbf887d71d92c70297bfb422b175ef5e249a
Benjamin Crozat <benjamincrozat@me.com>
2023-03-11 12:03:41 +0100

    WIP

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -15,7 +15,7 @@
             <ul class="grid gap-2 mt-4">
                 <li>
                     <a href="https://blogging-with-laravel.com" class="decoration-white/30 underline underline-offset-4">
-                        Blogging with Laravel Masterclass
+                        Blogging with Laravel
 
                         <span class="-mr-2 bg-gradient-to-r from-indigo-500 to-indigo-600 inline-block leading-tight px-3 py-1 rounded-full scale-75 text-white text-xs uppercase">
                             New
