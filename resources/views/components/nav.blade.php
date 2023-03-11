89c43ebb3df1c70200448e1c8546d0857ec74ef2
Benjamin Crozat <benjamincrozat@me.com>
2023-03-11 11:56:42 +0100

    WIP

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,24 +1,26 @@
-<a href="https://blogging-with-laravel.com" class="bg-gradient-to-r from-gray-900 dark:from-gray-800/50 to-gray-700 dark:to-gray-700/50 block">
-    <div class="container flex items-center justify-between gap-4 leading-tight py-4 text-sm text-white">
-        <div>
-            <div class="text-gray-200">
-                Build a better blog with <span class="bg-clip-text bg-gradient-to-r from-red-300 to-red-400 font-normal text-transparent">Laravel</span>. Get <span class="bg-clip-text bg-gradient-to-r from-orange-300 to-orange-400 font-normal text-transparent">10K monthly visitors</span> from Google in just 6 months.
+@empty($funnel)
+    <a href="https://blogging-with-laravel.com" class="bg-gradient-to-r from-gray-900 dark:from-gray-800/50 to-gray-700 dark:to-gray-700/50 block">
+        <div class="container flex items-center justify-between gap-4 leading-tight py-4 text-sm text-white">
+            <div>
+                <div class="text-gray-200">
+                    Build a better blog with <span class="bg-clip-text bg-gradient-to-r from-red-300 to-red-400 font-normal text-transparent">Laravel</span>. Get <span class="bg-clip-text bg-gradient-to-r from-orange-300 to-orange-400 font-normal text-transparent">10K monthly visitors</span> from Google in just 6 months.
 
-                <div class="flex items-center gap-[.35rem] mt-2 text-blue-400">
-                    Pre-order now
+                    <div class="flex items-center gap-[.35rem] mt-2 text-blue-400">
+                        Pre-order now
 
-                    <x-heroicon-o-arrow-right class="w-3 h-3" />
+                        <x-heroicon-o-arrow-right class="w-3 h-3" />
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="bg-white/10 flex-shrink-0 overflow-hidden relative rounded-full w-16 h-16">
-            <x-icon-blogging-with-laravel
-                class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-2 w-24 h-24"
-            />
+            <div class="bg-white/10 flex-shrink-0 overflow-hidden relative rounded-full w-16 h-16">
+                <x-icon-blogging-with-laravel
+                    class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-2 w-24 h-24"
+                />
+            </div>
         </div>
-    </div>
-</a>
+    </a>
+@endempty
 
 <div {{ $attributes->merge(['class' => 'container flex items-center justify-between'])}}>
     <a
@@ -34,7 +36,7 @@
         </span>
     </a>
 
-    @if (empty($funnel))
+    @empty($funnel)
         <div class="flex items-center gap-2">
             <button class="flex items-center gap-2 group hover:text-indigo-400 transition-colors" @click="searching = true; window.fathom?.trackGoal('NV4ZNM3W', 0)">
                 <x-heroicon-s-magnifying-glass class="-translate-y-[.5px] sm:-translate-y-0 flex-shrink-0 w-4 h-4" />
@@ -45,5 +47,5 @@
                 ⌘K
             </span>
         </div>
-    @endif
+    @endempty
 </div>
