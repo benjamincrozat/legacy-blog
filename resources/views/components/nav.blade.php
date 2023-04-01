82ec7fe5348458bc026d33764372320e11059d03
Benjamin Crozat <benjamincrozat@me.com>
2023-04-01 21:13:17 +0200

    WIP

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,35 +1,65 @@
 @empty($funnel)
-    <a href="https://blogging-with-laravel.com" class="bg-gradient-to-r from-gray-900 dark:from-gray-800/50 to-gray-700 dark:to-gray-700/50 block">
-        <div class="container flex items-center justify-between gap-4 leading-tight py-4 text-sm text-white">
-            <div class="text-gray-200">
-                <div>
-                    Maximize your <span class="bg-clip-text bg-gradient-to-r from-red-300 to-red-400 font-normal text-transparent">Laravel</span> blog's potential with <span class="bg-clip-text bg-gradient-to-r from-green-300 to-green-400 font-normal text-transparent">SEO best practices</span> and reach <span class="bg-clip-text bg-gradient-to-r from-orange-300 to-orange-400 font-normal text-transparent">10K monthly clicks</span> on Google.
+    @php $switch = (bool) mt_rand(0, 1); @endphp
+
+    @if ($switch)
+        <a href="https://smousss.com" class="bg-gradient-to-r from-gray-900 dark:from-gray-800/50 to-gray-700 dark:to-gray-700/50 block">
+            <div class="container flex items-center justify-between gap-4 leading-tight py-4 text-sm text-white">
+                <div class="text-gray-200">
+                    <div>
+                        <strong class="font-medium">Laravel developers</strong>, <span class="bg-clip-text bg-gradient-to-r from-orange-400 to-yellow-400 text-transparent">let GPT-4 handle repetitive tasks.</span><br class="hidden sm:inline" />
+
+                        <span class="bg-clip-text bg-gradient-to-r from-indigo-200 to-blue-300 text-transparent">Admin panels creation, REST API docs and i18nization, 100% handled by artificial intelligence.</span>
+                    </div>
+
+                    <div class="flex items-center gap-[.35rem] mt-2 text-blue-400">
+                        <span>Try for free</span>
+                        <x-heroicon-o-arrow-right class="w-3 h-3" />
+                    </div>
                 </div>
 
-                <div class="flex items-center gap-[.35rem] mt-2 text-blue-400">
-                    <span>Preview the course for free</span>
-                    <x-heroicon-o-arrow-right class="w-3 h-3" />
-                </div>
-            </div>
-
-            <div class="flex-shrink-0 relative">
                 <img
                     loading="lazy"
-                    src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}?s=128"
+                    src="{{ Vite::asset('resources/img/smousss.png') }}"
                     width="64"
                     height="64"
-                    alt="Benjamin Crozat"
-                    class="rounded-full"
+                    alt="Smousss"
+                    class="flex-shrink-0 rounded-full"
                 />
-
-                <span class="absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-[30%]
-                bg-gradient-to-r from-indigo-400 to-blue-300 font-bold inline-block
-                leading-tight px-2 py-1 rounded-full scale-[.65] text-xs uppercase">
-                    New!
-                </span>
             </div>
-        </div>
-    </a>
+        </a>
+    @else
+        <a href="https://blogging-with-laravel.com" class="bg-gradient-to-r from-gray-900 dark:from-gray-800/50 to-gray-700 dark:to-gray-700/50 block">
+            <div class="container flex items-center justify-between gap-4 leading-tight py-4 text-sm text-white">
+                <div class="text-gray-200">
+                    <div>
+                        Maximize your <span class="bg-clip-text bg-gradient-to-r from-red-300 to-red-400 font-normal text-transparent">Laravel</span> blog's potential with <span class="bg-clip-text bg-gradient-to-r from-green-300 to-green-400 font-normal text-transparent">SEO best practices</span> and reach <span class="bg-clip-text bg-gradient-to-r from-orange-300 to-orange-400 font-normal text-transparent">10K monthly clicks</span> on Google.
+                    </div>
+
+                    <div class="flex items-center gap-[.35rem] mt-2 text-blue-400">
+                        <span>Preview the course for free</span>
+                        <x-heroicon-o-arrow-right class="w-3 h-3" />
+                    </div>
+                </div>
+
+                <div class="flex-shrink-0 relative">
+                    <img
+                        loading="lazy"
+                        src="https://www.gravatar.com/avatar/{{ md5('benjamincrozat@me.com') }}?s=128"
+                        width="64"
+                        height="64"
+                        alt="Benjamin Crozat"
+                        class="rounded-full"
+                    />
+
+                    <span class="absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-[30%]
+                    bg-gradient-to-r from-indigo-400 to-blue-300 font-bold inline-block
+                    leading-tight px-2 py-1 rounded-full scale-[.65] text-xs uppercase">
+                        New!
+                    </span>
+                </div>
+            </div>
+        </a>
+    @endif
 @endempty
 
 <div {{ $attributes->merge(['class' => 'container flex items-center justify-between'])}}>
