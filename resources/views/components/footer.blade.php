11134cf3b146ab3c85f44ad885b47fcd5a63f029
Benjamin Crozat <benjamincrozat@me.com>
2023-06-22 10:02:07 +0200

    WIP

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,47 +1,27 @@
-<footer {{ $attributes->merge(['class' => 'container py-8 md:py-16 text-sm']) }} x-intersect="window.fathom?.trackGoal('08VVENFW', 0)">
-    <div class="grid gap-8 md:gap-16 md:grid-cols-2">
-        <div class="col-span-1">
-            <div class="font-semibold text-white">My other projects</div>
+<footer {{ $attributes->merge(['class' => 'container py-8 text-sm']) }} x-intersect="window.fathom?.trackGoal('08VVENFW', 0)">
+    <p class="text-center">
+        <span class="opacity-75">This blog is hosted by</span> <a href="https://benjamincrozat.com/recommends/digitalocean" target="_blank" rel="nofollow noopener noreferrer">DigitalOcean</a> <span class="opacity-75">and Laravel Forge. Analytics are provided by</span> <a href="https://benjamincrozat.com/recommends/fathom-analytics" target="_blank" rel="nofollow noopener noreferrer">Fathom</a><span class="opacity-75">.</span>
+    </p>
 
-            <ul class="grid gap-2 mt-4">
-                <li>
-                    <a href="https://bloggingwithlaravel.com" class="underline decoration-white/30 underline-offset-4">
-                        Blogging with Laravel
-                    </a>
-                </li>
-
-                <li>
-                    <a href="https://smousss.com" class="underline decoration-white/30 underline-offset-4">
-                        Smousss, an AI assistant for Laravel developers
-                    </a>
-                </li>
-            </ul>
+    <div class="flex flex-wrap items-center justify-between mt-8 sm:flex-nowrap">
+        <div class="w-full text-xs tracking-widest text-center uppercase opacity-50 sm:w-auto">
+            © Benjamin Crozat {{ date('Y') }}. All&nbsp;rights&nbsp;reserved.
         </div>
 
-        <div class="col-span-1">
-            <div class="font-semibold text-white">Follow me</div>
+        <ul class="flex items-center justify-center w-full gap-2 mt-10 sm:justify-start sm:mt-0 sm:w-auto">
+            <li>
+                <a href="https://github.com/benjamincrozat" target="_blank" rel="nofollow noopener noreferrer" class="underline decoration-white/30 underline-offset-4">
+                    <x-icon-github class="w-8 h-8 fill-current" />
+                    <span class="sr-only">GitHub</span>
+                </a>
+            </li>
 
-            <ul class="grid gap-2 mt-4">
-                <li>
-                    <a href="https://github.com/benjamincrozat" target="_blank" rel="nofollow noopener noreferrer" class="underline decoration-white/30 underline-offset-4">
-                        GitHub
-                    </a>
-                </li>
-
-                <li>
-                    <a href="https://twitter.com/benjamincrozat" target="_blank" rel="nofollow noopener noreferrer" class="underline decoration-white/30 underline-offset-4">
-                        Twitter
-                    </a>
-                </li>
-            </ul>
-        </div>
+            <li>
+                <a href="https://twitter.com/benjamincrozat" target="_blank" rel="nofollow noopener noreferrer" class="underline decoration-white/30 underline-offset-4">
+                    <x-icon-twitter class="w-8 h-8 fill-current" />
+                    <span class="sr-only">Twitter</span>
+                </a>
+            </li>
+        </ul>
     </div>
-
-    <p class="mt-8 text-center md:mt-16">
-        <span class="opacity-50">Hosting by</span> <a href="https://benjamincrozat.com/recommends/digitalocean" target="_blank" rel="nofollow noopener noreferrer">DigitalOcean</a> <span class="opacity-50">and analytics by</span> <a href="https://benjamincrozat.com/recommends/fathom-analytics" target="_blank" rel="nofollow noopener noreferrer">Fathom</a><span class="opacity-50">.</span>
-    </p>
-
-    <p class="mt-8 text-xs tracking-widest text-center uppercase opacity-50">
-        © Benjamin Crozat {{ date('Y') }}. All&nbsp;rights&nbsp;reserved.
-    </p>
 </footer>
