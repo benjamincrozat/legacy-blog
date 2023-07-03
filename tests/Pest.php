e394c27f123782656a37dcd963828cedfbab8eb9
Benjamin Crozat <benjamincrozat@me.com>
2023-07-03 10:26:02 +0200

    WIP

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Stringable;
 use Illuminate\Support\Facades\Http;
 use App\CommonMark\MarxdownConverter;
 use function Pest\Laravel\withoutVite;
+use Torchlight\Middleware\RenderTorchlight;
+use function Pest\Laravel\withoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 uses(TestCase::class, DatabaseTransactions::class)->in('Feature');
@@ -12,10 +14,12 @@ uses(TestCase::class, DatabaseTransactions::class)->in('Feature');
 beforeEach(function () {
     Http::preventStrayRequests();
 
-    withoutVite();
-
     Stringable::macro(
         'marxdown',
         fn () => (new MarxdownConverter(torchlight: false))->convert($this->value)
     );
+
+    withoutMiddleware(RenderTorchlight::class);
+
+    withoutVite();
 });
