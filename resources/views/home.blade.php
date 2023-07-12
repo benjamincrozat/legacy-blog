991b2942d45e409b70b02bddd0d87a0a0b863cf9
Benjamin Crozat <benjamincrozat@me.com>
2023-07-12 19:53:46 +0200

    WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,7 +67,7 @@
                 @foreach ($posts as $post)
                     <x-post :post="$post" />
 
-                    @if ($counter > 0)
+                    @if ($counter > 0 && (bool) mt_rand(0, 1))
                         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3461630254419592" crossorigin="anonymous"></script>
                         <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-g7-26-1h-5n+yf" data-ad-client="ca-pub-3461630254419592" data-ad-slot="7752607445"></ins>
                         <script>(adsbygoogle = window.adsbygoogle || []).push({})</script>
