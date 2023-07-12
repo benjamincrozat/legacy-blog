702ddff2270ea015db9958cbd47143c1c7aec07f
Benjamin Crozat <benjamincrozat@me.com>
2023-07-12 19:52:51 +0200

    WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,8 +60,22 @@
 
         @if ($posts->isNotEmpty())
             <div class="grid gap-4 mt-8 md:grid-cols-2">
+                @php
+                $counter = 5;
+                @endphp
+
                 @foreach ($posts as $post)
                     <x-post :post="$post" />
+
+                    @if ($counter > 0)
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3461630254419592" crossorigin="anonymous"></script>
+                        <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-g7-26-1h-5n+yf" data-ad-client="ca-pub-3461630254419592" data-ad-slot="7752607445"></ins>
+                        <script>(adsbygoogle = window.adsbygoogle || []).push({})</script>
+
+                        @php
+                        $counter--;
+                        @endphp
+                    @endif
                 @endforeach
             </div>
 
