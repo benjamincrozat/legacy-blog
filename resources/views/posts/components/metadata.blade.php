e63c56b422863e286cc183c150ea975181815c36
Benjamin Crozat <benjamincrozat@me.com>
2023-07-21 12:50:31 +0200

    Updated metadata.blade.php

--- resources/views/posts/components/metadata.blade.php
+++ resources/views/posts/components/metadata.blade.php
@@ -1,4 +1,9 @@
 <div {{ $attributes->merge(['class' => 'text-sm']) }}>
-    <a href="{{ $twitterUrl }}" class="font-normal underline">{{ $name }}</a>
-    — Updated on <time datetime="{{ $modifiedAt?->toDateTimeString() }}">{{ $modifiedAt?->isoFormat('ll') }}</time>
+    <a href="{{ $twitterUrl }}" class="font-normal underline"><img
+        src="https://www.gravatar.com/avatar/{{ md5($email) }}?s=40"
+        width="40"
+        height="40"
+        alt="{{ $name }}"
+        class="mr-1 -translate-y-px w-[20px] h-[20px] rounded-full inline"
+    /> {{ $name }}</a> — Updated on <time datetime="{{ $modifiedAt?->toDateTimeString() }}">{{ $modifiedAt?->isoFormat('ll') }}</time>
 </div>
