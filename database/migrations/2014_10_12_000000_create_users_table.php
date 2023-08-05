84d6b4c1673549554822e05ed23451db0b130d44
Benjamin Crozat <benjamincrozat@me.com>
2023-08-05 09:17:57 +0200

    WIP

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
         if (! app()->runningUnitTests()) {
             User::factory()->create([
                 'name' => 'Benjamin Crozat',
-                'email' => 'benjamincrozat@me.com',
+                'email' => 'hello@benjamincrozat.com',
             ]);
         }
     }
