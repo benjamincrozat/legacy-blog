2585fe1a27d5d2d4d32811f2253d229f20f2ed16
Benjamin Crozat <benjamincrozat@me.com>
2023-08-06 23:56:31 +0200

    WIP

--- app/Actions/GeneratePostTeaser.php
+++ app/Actions/GeneratePostTeaser.php
@@ -25,7 +25,7 @@ class GeneratePostTeaser
 
 ---
 
-Tease this article without giving away the crucial details. Give the user a reason to click, but don't overdo it. Your tone must be the same as in the article and you must avoid repetitions. Speak using the first person as if you were the author.
+Tease this article without giving away the crucial details. Give the user a reason to click, but don't overdo it. Your tone must be the same as in the article and you must avoid repetitions. Add a line break after each sentence. Speak using the first person as if you were the author.
 PROMPT,
                     ],
                 ],
