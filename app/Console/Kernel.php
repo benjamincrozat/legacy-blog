46592e7cc1633147b9e432c0684b12b799f29f1d
Benjamin Crozat <benjamincrozat@me.com>
2023-08-09 13:39:06 +0200

    Sitemaps are useless with good internal linking

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,17 +4,12 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use App\Console\Commands\AnalyticsFetchCommand;
-use App\Console\Commands\SitemapGenerateCommand;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule) : void
     {
-        $schedule
-            ->command(SitemapGenerateCommand::class)
-            ->daily();
-
         $schedule
             ->command(AnalyticsFetchCommand::class)
             ->mondays();
