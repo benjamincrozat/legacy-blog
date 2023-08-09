b5e1af4905ae30da441754e848b58f47777c67ee
Benjamin Crozat <benjamincrozat@me.com>
2023-08-09 12:53:38 +0200

    WIP

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,13 +1,8 @@
 <button {{ $attributes->except('@click')->merge([
     'class' => 'flex items-center gap-2 transition-colors group hover:text-indigo-400',
 ]) }} @click="searching = ! searching">
+    <span class="sr-only">Search</span>
     <x-heroicon-s-magnifying-glass class="flex-shrink-0 w-4 h-4" />
-
-    Search
-
-    <span class="border dark:border-gray-800 inline-block px-2 py-[.35rem] rounded scale-[.8] !text-gray-600 dark:!text-gray-300 text-xs sm:translate-y-px uppercase">
-        ⌘K
-    </span>
 </button>
 
 @if (Route::is('home') || Route::is('posts.show'))
