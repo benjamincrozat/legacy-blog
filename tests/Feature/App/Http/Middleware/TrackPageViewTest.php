524c99abc011b62079373e7f9921731ec078ab03
Benjamin Crozat <benjamincrozat@me.com>
2023-08-09 12:48:03 +0200

    WIP

--- tests/Feature/App/Http/Middleware/TrackPageViewTest.php
+++ tests/Feature/App/Http/Middleware/TrackPageViewTest.php
@@ -4,6 +4,7 @@ use App\Models\User;
 use App\Models\Affiliate;
 use function Pest\Laravel\get;
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\postJson;
 use Illuminate\Support\Facades\Queue;
 use App\Http\Middleware\TrackPageView;
 use function Pest\Laravel\withMiddleware;
@@ -22,16 +23,22 @@ it('tracks page views', function () {
     Queue::assertPushed(\App\Jobs\TrackPageView::class);
 });
 
-it('does not track the page view', function ($path, $requiresAuthentication) {
+it('does not track the page view for excluded paths', function ($path, $requiresAuthentication) {
     if ($requiresAuthentication) {
         actingAs(User::factory()->create());
     }
 
     get($path);
 
-    Queue::assertNothingPushed();
+    Queue::assertNotPushed(\App\Jobs\TrackPageView::class);
 })->with([
     ['/horizon', true],
     ['/nova', true],
     fn () => [route('affiliate.show', Affiliate::factory()->create()), false],
 ]);
+
+it('does not track page views for HTTP methods other than GET', function () {
+    postJson(route('subscribe'));
+
+    Queue::assertNotPushed(\App\Jobs\TrackPageView::class);
+});
