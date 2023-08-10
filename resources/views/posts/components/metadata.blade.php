bd0e97f4664008246c4f46a8fb4f331a367e078c
Benjamin Crozat <benjamincrozat@me.com>
2023-08-10 10:10:12 +0200

    WIP

--- resources/views/posts/components/metadata.blade.php
+++ resources/views/posts/components/metadata.blade.php
@@ -1,9 +1,9 @@
 <div {{ $attributes->merge(['class' => 'text-sm']) }}>
-    <a href="{{ $twitterUrl }}" class="font-normal underline"><img
+    <a href="{{ $twitterUrl }}" class="font-medium"><img
         src="{{ $gravatar }}?s=40"
         width="40"
         height="40"
         alt="{{ $name }}"
-        class="mr-1 -translate-y-px w-[20px] h-[20px] rounded-full inline"
-    /> {{ $name }}</a> — Updated on <time datetime="{{ $modifiedAt?->toDateTimeString() }}">{{ $modifiedAt?->isoFormat('ll') }}</time>
+        class="mr-2 -translate-y-[1.5px] w-[20px] h-[20px] rounded-full inline"
+    />{{ $name }}</a> — Updated on <time datetime="{{ $modifiedAt?->toDateTimeString() }}">{{ $modifiedAt?->isoFormat('ll') }}</time>
 </div>
