54164f407541b2f749989b64d9ac489af1c1c433
Benjamin Crozat <benjamincrozat@me.com>
2023-08-12 17:44:53 +0200

    WIP

--- app/Http/Middleware/ApplyRedirectionRule.php
+++ app/Http/Middleware/ApplyRedirectionRule.php
@@ -9,7 +9,7 @@ class ApplyRedirectionRule
 {
     public function handle(Request $request, \Closure $next) : mixed
     {
-        if ($redirect = Redirect::where('from', 'REGEXP', '^/?' . $request->path() . '$')->first()) {
+        if ($redirect = Redirect::where('from', 'REGEXP', $request->path() . '$')->first()) {
             return redirect(trim($redirect->to, '/'), 301);
         }
 
--- tests/Feature/App/Http/Middleware/ApplyRedirectionRuleTest.php
+++ tests/Feature/App/Http/Middleware/ApplyRedirectionRuleTest.php
@@ -13,3 +13,19 @@ test('middleware applies existing redirections rules', function () {
         ->assertStatus(301)
         ->assertRedirect($redirect->to);
 });
+
+test('middleware does not initiate an infinite loop of redirects', function () {
+    $redirect = Redirect::create([
+        'from' => '/foo-bar',
+        'to' => '/foo',
+    ]);
+
+    get($redirect->from)
+        ->assertStatus(301)
+        ->assertRedirect($redirect->to);
+
+    get($redirect->to)
+        // This route does not exist, so a 404 is to be expected.
+        // If the bug was still happening, I'd have a redirect.
+        ->assertNotFound();
+});
