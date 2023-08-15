d1a79f301624a2b7c8d02c0226225c695186fbff
Benjamin Crozat <benjamincrozat@me.com>
2023-08-15 13:43:09 +0200

    Revert "Fixed security issue for wildcard routes"
    This reverts commit b897497cc369b26d2db936d392c7d5732d060353.

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,4 @@ Route::post('/logout', LogoutController::class)->name('logout');
 Route::view('/terms-of-service', 'terms-of-service')->name('terms-of-service');
 
 // This is a wildcard route, so it should come last.
-Route::get('/{post:slug}', ShowPostController::class)
-    ->name('posts.show')
-    ->where('post', '[\w\d-]+');
+Route::get('/{post:slug}', ShowPostController::class)->name('posts.show');
