3210bc7c2c06891710aeb5576f18ead187097754
Benjamin Crozat <benjamincrozat@me.com>
2023-08-24 01:49:46 +0200

    WIP

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,6 +1,6 @@
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between mt-16">
-        <div class="text-gray-400 dark:text-gray-700">
+        <div class="text-gray-400">
             {{ $paginator->count() }}
             {!! __('of') !!}
             <span>{{ $paginator->total() }}</span>
@@ -9,18 +9,18 @@
 
         <div class="flex gap-1">
             @if ($paginator->onFirstPage())
-                <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}" class="flex items-center justify-center w-8 h-8 text-gray-200 cursor-default dark:text-gray-800">
+                <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}" class="flex items-center justify-center w-8 h-8 text-gray-200 cursor-default">
                     ←
                 </span>
             @else
-                <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="flex items-center justify-center w-8 h-8 text-blue-400 transition-colors bg-gray-100 rounded-sm dark:bg-gray-800/50 hover:bg-gray-200/50 hover:dark:bg-gray-800 hover:text-blue-500">
+                <a wire:navigate href="{{ $paginator->previousPageUrl() }}" rel="prev" class="flex items-center justify-center w-8 h-8 text-blue-400 transition-colors bg-gray-100 rounded-sm hover:bg-gray-200/50 hover:text-blue-500">
                     ←
                 </a>
             @endif
 
             @foreach ($elements as $element)
                 @if (is_string($element))
-                    <span aria-disabled="true" class="flex items-center justify-center w-8 h-8 text-gray-200 cursor-default dark:text-gray-800">
+                    <span aria-disabled="true" class="flex items-center justify-center w-8 h-8 text-gray-200 cursor-default">
                         <span>{{ $element }}</span>
                     </span>
                 @endif
@@ -28,11 +28,11 @@
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <span aria-current="page" class="flex items-center justify-center w-8 h-8 bg-gray-200 rounded-sm cursor-default dark:bg-gray-800">
+                            <span aria-current="page" class="flex items-center justify-center w-8 h-8 bg-gray-200 rounded-sm cursor-default">
                                 <span>{{ $page }}</span>
                             </span>
                         @else
-                            <a href="{{ $url }}" class="flex items-center justify-center w-8 h-8 text-blue-400 transition-colors bg-gray-100 rounded-sm dark:bg-gray-800/50 hover:bg-gray-200/50 hover:dark:bg-gray-800 hover:text-blue-500">
+                            <a wire:navigate href="{{ $url }}" class="flex items-center justify-center w-8 h-8 text-blue-400 transition-colors bg-gray-100 rounded-sm hover:bg-gray-200/50 hover:text-blue-500">
                                 {{ $page }}
                             </a>
                         @endif
@@ -41,11 +41,11 @@
             @endforeach
 
             @if ($paginator->hasMorePages())
-                <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="flex items-center justify-center w-8 h-8 text-blue-400 transition-colors bg-gray-100 rounded-sm dark:bg-gray-800/50 hover:bg-gray-200/50 hover:dark:bg-gray-800 hover:text-blue-500">
+                <a wire:navigate href="{{ $paginator->nextPageUrl() }}" rel="next" class="flex items-center justify-center w-8 h-8 text-blue-400 transition-colors bg-gray-100 rounded-sm hover:bg-gray-200/50 hover:text-blue-500">
                     →
                 </a>
             @else
-                <span aria-disabled="true" aria-label="{{ __('pagination.next') }}" class="flex items-center justify-center w-8 h-8 text-gray-200 rounded-sm cursor-default dark:text-gray-800">
+                <span aria-disabled="true" aria-label="{{ __('pagination.next') }}" class="flex items-center justify-center w-8 h-8 text-gray-200 rounded-sm cursor-default">
                     →
                 </span>
             @endif
