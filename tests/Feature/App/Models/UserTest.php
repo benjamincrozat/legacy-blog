e50260810a65a9c6a089a775f27e68f11254ce4f
Benjamin Crozat <benjamincrozat@me.com>
2023-08-23 14:16:20 +0200

    WIP

--- tests/Feature/App/Models/UserTest.php
+++ tests/Feature/App/Models/UserTest.php
@@ -3,9 +3,9 @@
 use App\Models\User;
 
 test('description is rendered and cached after saving', function () {
-    $model = User::factory()->create();
+    $user = User::factory()->create();
 
     expect(cache()->has(
-        'User.1.description.' . sha1($model->description)
+        "User.$user->id.description." . sha1($user->description)
     ))->toBeTrue();
 });
