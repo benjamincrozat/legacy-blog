e697fa09c3b2977f656ceca00e023bb3c042a8f2
Benjamin Crozat <benjamincrozat@me.com>
2023-08-24 10:18:20 +0200

    WIP

--- tests/Feature/Views/Components/AppTest.php
+++ tests/Feature/Views/Components/AppTest.php
@@ -1,5 +1,6 @@
 <?php
 
+// Test the components.app view works as expected. It's a critical piece of code.
 it('renders', function () {
     /** @var \NunoMaduro\LaravelMojito\ViewAssertion */
     $view = $this->assertView('components.app');
@@ -10,6 +11,9 @@ it('renders', function () {
         'name' => 'viewport',
         'content' => 'width=device-width, initial-scale=1, viewport-fit=cover',
     ]);
+
+    // Make sure the Tailwind CSS Play CDN is loaded and
+    // adds the CSS for the values stored in database.
 
     $view->has('script[src="https://cdn.tailwindcss.com"]');
 
