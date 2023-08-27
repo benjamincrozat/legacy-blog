e6224c87fbd56fedf9bb2d3d35f90720dd920869
Benjamin Crozat <benjamincrozat@me.com>
2023-08-27 11:38:26 +0200

    WIP

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,64 +1,65 @@
 <html lang="{{ app()->getLocale() }}" class="scroll-smooth">
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-    <meta name="description" content="{{ $description ?? '' }}" />
-    <meta property="og:title" content="{{ $title ?? config('app.name') }}" />
-    @if (! empty($image))
-        <meta property="og:image" content="{{ $image }}" />
-    @endif
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="{{ url()->current() }}" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:creator" content="@benjamincrozat" />
-    <meta name="twitter:description" content="{{ $description ?? '' }}" />
-    @if (! empty($image))
-        <meta name="twitter:image" content="{{ $image }}" />
-    @endif
-    <meta name="twitter:title" content="{{ $title ?? config('app.name') }}" />
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="description" content="{{ $description ?? '' }}" />
+        <meta property="og:title" content="{{ $title ?? config('app.name') }}" />
+        @if (! empty($image))
+            <meta property="og:image" content="{{ $image }}" />
+        @endif
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="{{ url()->current() }}" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@benjamincrozat" />
+        <meta name="twitter:description" content="{{ $description ?? '' }}" />
+        @if (! empty($image))
+            <meta name="twitter:image" content="{{ $image }}" />
+        @endif
+        <meta name="twitter:title" content="{{ $title ?? config('app.name') }}" />
 
-    <title>{{ $title ?? config('app.name') }}</title>
+        <title>{{ $title ?? config('app.name') }}</title>
 
-    @unless(app()->runningUnitTests())
-        @googlefonts
-    @endunless
+        @unless(app()->runningUnitTests())
+            @googlefonts
+        @endunless
 
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-            corePlugins: [
-                'accentColor',
-                'backgroundColor',
-                'borderColor',
-                'boxShadowColor',
-                'caretColor',
-                'divideColor',
-                'gradientColorStops',
-                'outlineColor',
-                'placeholderColor',
-                'ringColor',
-                'ringOffsetColor',
-                'textDecorationColor',
-                'textColor',
-            ],
-        }
-    </script>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+            tailwind.config = {
+                corePlugins: [
+                    'accentColor',
+                    'backgroundColor',
+                    'borderColor',
+                    'boxShadowColor',
+                    'caretColor',
+                    'divideColor',
+                    'gradientColorStops',
+                    'outlineColor',
+                    'placeholderColor',
+                    'ringColor',
+                    'ringOffsetColor',
+                    'textDecorationColor',
+                    'textColor',
+                ],
+            }
+        </script>
 
-    @livewireStyles
+        @livewireStyles
 
-    @livewireScripts
+        @livewireScripts
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <x-feed-links />
+        <x-feed-links />
 
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/img/apple-touch-icon.jpg') }}" />
-    <link rel="icon" type="image/jpeg" sizes="16x16" href="{{ Vite::asset('resources/img/16x16.jpg') }}" />
-    <link rel="icon" type="image/jpeg" sizes="32x32" href="{{ Vite::asset('resources/img/32x32.jpg') }}" />
-    <link rel="icon" type="image/jpeg" sizes="48x48" href="{{ Vite::asset('resources/img/48x48.jpg') }}" />
-    <link rel="icon" type="image/jpeg" sizes="96x96" href="{{ Vite::asset('resources/img/96x96.jpg') }}" />
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/img/apple-touch-icon.jpg') }}" />
+        <link rel="icon" type="image/jpeg" sizes="16x16" href="{{ Vite::asset('resources/img/16x16.jpg') }}" />
+        <link rel="icon" type="image/jpeg" sizes="32x32" href="{{ Vite::asset('resources/img/32x32.jpg') }}" />
+        <link rel="icon" type="image/jpeg" sizes="48x48" href="{{ Vite::asset('resources/img/48x48.jpg') }}" />
+        <link rel="icon" type="image/jpeg" sizes="96x96" href="{{ Vite::asset('resources/img/96x96.jpg') }}" />
 
-    <link rel="canonical" href="{{ $canonical ?? url()->current() }}" />
-
+        <link rel="canonical" href="{{ $canonical ?? url()->current() }}" />
+    </head>
     <body {{ $attributes->except(['description', 'image', 'title', 'canonical'])->merge([
         'class' => 'bg-gray-50 font-light',
         'x-data' => '{}',
