e0e72f78b3d8b0f086c6339b092b404c5005d747
Benjamin Crozat <benjamincrozat@me.com>
2023-08-27 23:48:45 +0200

    They weren't necessary as actions

--- app/Jobs/GeneratePostDescription.php
+++ app/Jobs/GeneratePostDescription.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use App\Models\Post;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -19,8 +20,60 @@ class GeneratePostDescription implements ShouldQueue
     ) {
     }
 
-    public function handle() : void
+    public function handle()
     {
-        (new \App\Actions\GeneratePostDescription)->generate($this->post, $this->model);
+        $response = Http::withToken(config('services.openai.api_key'))
+            ->timeout(300)
+            ->baseUrl('https://api.openai.com/v1')
+            ->post('/chat/completions', [
+                'model' => $this->model,
+                'messages' => [
+                    [
+                        'role' => 'user',
+                        'content' => $this->post->community_link
+                            ? <<<PROMPT
+# {$this->post->title}
+{$this->post->content}
+
+---
+
+Context: This is a community link I shared.
+
+Instructions:
+- Summarize it in the shortest form possible.
+- Don't reuse the title.
+- Use a natural tone.
+- Speak in the first person as if you were the person who shared.
+- Give the user a reason to click, but don't overdo it.
+- Use 20 words or less.
+PROMPT
+                            : <<<PROMPT
+# {$this->post->title}
+{$this->post->introduction}
+{$this->post->content}
+{$this->post->conclusion}
+
+---
+
+Context: This is an article I wrote.
+
+Instructions:
+- Summarize it in the shortest form possible.
+- Don't reuse the title.
+- Use the same tone as in the article.
+- Speak in the first person as if you were the author.
+- Give the user a reason to click, but don't overdo it.
+- Use 20 words or less.
+PROMPT,
+                    ],
+                ],
+                'max_tokens' => 64,
+            ])
+            ->throw()
+            ->json();
+
+        $this->post->update([
+            'description' => trim(trim($response['choices'][0]['message']['content']), '"\''),
+        ]);
     }
 }
--- app/Jobs/GeneratePostTeaser.php
+++ app/Jobs/GeneratePostTeaser.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use App\Models\Post;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -21,6 +22,31 @@ class GeneratePostTeaser implements ShouldQueue
 
     public function handle() : void
     {
-        (new \App\Actions\GeneratePostTeaser)->generate($this->post, $this->model);
+        $response = Http::withToken(config('services.openai.api_key'))
+            ->timeout(300)
+            ->baseUrl('https://api.openai.com/v1')
+            ->post('/chat/completions', [
+                'model' => $this->model,
+                'messages' => [
+                    [
+                        'role' => 'user',
+                        'content' => <<<PROMPT
+{$this->post->title}
+{$this->post->content}
+
+---
+
+Tease this article without giving away the crucial details. Give the user a reason to click, but don't overdo it. Your tone must be the same as in the article and you must avoid repetitions. Speak using the first person as if you were the author.
+PROMPT,
+                    ],
+                ],
+                'max_tokens' => 1024,
+            ])
+            ->throw()
+            ->json();
+
+        $this->post->update([
+            'teaser' => trim(trim($response['choices'][0]['message']['content']), '"\''),
+        ]);
     }
 }
