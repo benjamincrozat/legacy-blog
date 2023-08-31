89aeb4aa6703d859567a17f6988b5d6d98cbd9a2
Benjamin Crozat <benjamincrozat@me.com>
2023-08-31 10:57:10 +0200

    WIP

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -9,7 +9,7 @@
 <ol>
     <li>I launched this blog in September 2022 with the goal to be in everyone's Google search. <strong>I get more than 15,000 monthly clicks from it and more than 30,000 visits overall</strong> (<a href="https://benjamincrozat.pirsch.io/?domain=benjamincrozat.com&interval=30d&scale=day" target="_blank" rel="nofollow noopener noreferrer">my analytics dashboard is public</a> by the way).</li>
     <li>I also started growing my <a href="https://x.com/benjamincrozat" target="_blank" rel="nofollow noopener noreferrer">X (formerly Twitter)</a> account at the same time, which has now <strong>over 4,000 followers</strong>.</li>
-    <li>Jeffrey Way reached out and offered me to write for the <a href="https://laracasts.com/blog" target="_blank" rel="nofollow noopener noreferrer">blog of Laracasts</a>.</li>
+    <li>Then, Jeffrey Way took notice, reached out, and offered me to write for the <a href="https://laracasts.com/blog" target="_blank" rel="nofollow noopener noreferrer">blog of Laracasts</a>.</li>
 </ol>
 
 <p>Believe me, I'm just getting started and many other cool things are on the way!</p>
