bab96a2fd9f198ad88ee34c4edc47a3201661823
Benjamin Crozat <benjamincrozat@me.com>
2023-09-05 14:05:18 +0200

    Added first sponsor! 🥳 +$49 added to my MRR!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 <x-app
     title="Learn about Laravel and its ecosystem."
 >
-    <x-section class="container mt-24 text-center">
-        <x-slot:title tag="h1" class="font-bold text-center text-3xl/none md:text-4xl/none lg:text-5xl/none">
+    <x-section class="mt-24 text-center">
+        <x-slot:title tag="h1" class="container font-bold text-center text-3xl/none md:text-4xl/none lg:text-5xl/none">
             <div class="flex items-center justify-center gap-4 mt-8 md:gap-8">
                 <x-icon-laravel class="h-10 md:h-[3.25rem]" />
                 <x-icon-livewire class="h-10 mx-2 md:h-12" />
@@ -14,26 +14,26 @@
             <div class="mt-8">Learn about Laravel and its&nbsp;ecosystem.</div>
         </x-slot:title>
 
-        <p class="mt-2 text-xl/tight md:text-2xl/tight lg:text-3xl/tight">
+        <p class="container mt-2 text-xl/tight md:text-2xl/tight lg:text-3xl/tight">
             Join more than <span class="font-semibold text-transparent bg-gradient-to-r from-indigo-300 to-indigo-400 bg-clip-text">30,000</span> readers and skyrocket your web&nbsp;development&nbsp;skills.
         </p>
 
-        <div class="flex items-center justify-between gap-4 mt-16">
-            <x-icon-dashed-circle class="w-16 h-16 opacity-10" />
-            <x-icon-dashed-circle class="w-16 h-16 opacity-10" />
-            <x-icon-dashed-circle class="w-16 h-16 opacity-10" />
-            <x-icon-dashed-circle class="w-16 h-16 opacity-10" />
-            <x-icon-dashed-circle class="w-16 h-16 opacity-10" />
-            <x-icon-dashed-circle class="hidden w-16 h-16 sm:inline opacity-10" />
-            <x-icon-dashed-circle class="hidden w-16 h-16 sm:inline opacity-10" />
-            <x-icon-dashed-circle class="hidden w-16 h-16 md:inline opacity-10" />
-            <x-icon-dashed-circle class="hidden w-16 h-16 md:inline opacity-10" />
-            <x-icon-dashed-circle class="hidden w-16 h-16 lg:inline opacity-10" />
+        <div class="container flex items-center justify-between gap-4 mt-16 lg:max-w-screen-md">
+            <a href="https://useflipp.com/?utm_campaign=sponsorship&utm_source=benjamincrozat.com&utm_medium=logolink" target="_blank" rel="noopener">
+                <img loading="lazy" src="{{ Vite::asset('resources/img/flipp.png') }}" alt="Flipp" class="h-8 sm:h-10" />
+                <span class="sr-only">Flipp</span>
+            </a>
+            <x-icon-dashed-circle class="w-12 h-12 sm:w-16 sm:h-16 opacity-10" />
+            <x-icon-dashed-circle class="w-12 h-12 sm:w-16 sm:h-16 opacity-10" />
+            <x-icon-dashed-circle class="w-12 h-12 sm:w-16 sm:h-16 opacity-10" />
+            <x-icon-dashed-circle class="hidden w-12 h-12 sm:w-16 sm:h-16 sm:inline opacity-10" />
+            <x-icon-dashed-circle class="hidden w-12 h-12 sm:w-16 sm:h-16 sm:inline opacity-10" />
+            <x-icon-dashed-circle class="hidden w-12 h-12 sm:w-16 sm:h-16 md:inline opacity-10" />
         </div>
 
-        <div class="flex items-center justify-center gap-4 mt-8 text-3xl text-center font-handwriting">
+        <div class="flex items-center justify-center gap-4 mt-8 text-2xl text-center sm:text-3xl font-handwriting">
             <a wire:navigate.hover href="{{ route('media-kit') }}">
-                <x-icon-arrow-to-top-left class="inline w-8 h-8 -translate-y-3" /> <span class="underline decoration-1 underline-offset-2">Be the first to showcase your business!</span>
+                <x-icon-arrow-to-top-left class="inline w-8 h-8 -translate-y-3" /> <span class="underline decoration-1 underline-offset-2">Showcase your business too!</span>
             </a>
         </div>
     </x-section>
