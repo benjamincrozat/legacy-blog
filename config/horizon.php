f9ff451a7db7a34f64a9ffad36b14fd980d55ac6
Benjamin Crozat <benjamincrozat@me.com>
2023-09-06 14:36:25 +0200

    WIP

--- config/horizon.php
+++ config/horizon.php
@@ -196,9 +196,10 @@ return [
     'environments' => [
         'production' => [
             'supervisor-1' => [
-                'maxProcesses' => 10,
-                'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
+                'balanceMaxShift' => 1,
+                'maxProcesses' => 10,
+                'timeout' => 120,
             ],
         ],
 
