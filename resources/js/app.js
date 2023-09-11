08ebd43169d214dea82c9743e63e0253dacf9406
Benjamin Crozat <benjamincrozat@me.com>
2023-09-12 01:08:58 +0200

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,8 @@
 import 'highlight.js/styles/github-dark.css'
-import hljs from 'highlight.js/lib/core'
-import bash from 'highlight.js/lib/languages/bash'
-import blade from 'highlightjs-blade'
-import css from 'highlight.js/lib/languages/css'
-import diff from 'highlight.js/lib/languages/diff'
-import javascript from 'highlight.js/lib/languages/javascript'
-import json from 'highlight.js/lib/languages/json'
-import php from 'highlight.js/lib/languages/php'
-import sql from 'highlight.js/lib/languages/sql'
-import yaml from 'highlight.js/lib/languages/yaml'
+import hljs from 'highlight.js'
 import.meta.glob([
     '../img/**',
     '../svg/logo.svg',
 ])
-
-hljs.registerLanguage('bash', bash)
-hljs.registerLanguage('blade', blade)
-hljs.registerLanguage('css', css)
-hljs.registerLanguage('diff', diff)
-hljs.registerLanguage('javascript', javascript)
-hljs.registerLanguage('json', json)
-hljs.registerLanguage('php', php)
-hljs.registerLanguage('sql', sql)
-hljs.registerLanguage('yaml', yaml)
 
 window.hljs = hljs
