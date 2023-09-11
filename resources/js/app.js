3c665d60d20b5a6e13ba0231dad4080c5c55f539
Benjamin Crozat <benjamincrozat@me.com>
2023-09-12 00:48:24 +0200

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import 'highlight.js/styles/github-dark.css'
 import hljs from 'highlight.js/lib/core'
 import bash from 'highlight.js/lib/languages/bash'
 import diff from 'highlight.js/lib/languages/diff'
-import html from 'highlight.js/lib/languages/html'
 import javascript from 'highlight.js/lib/languages/javascript'
 import json from 'highlight.js/lib/languages/json'
 import php from 'highlight.js/lib/languages/php'
@@ -15,7 +14,6 @@ import.meta.glob([
 
 hljs.registerLanguage('bash', bash)
 hljs.registerLanguage('diff', diff)
-hljs.registerLanguage('html', html)
 hljs.registerLanguage('javascript', javascript)
 hljs.registerLanguage('json', json)
 hljs.registerLanguage('php', php)
