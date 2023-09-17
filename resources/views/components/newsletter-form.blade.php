fa3f69929dbafab902c35740d38526079eb77271
Benjamin Crozat <benjamincrozat@me.com>
2023-09-17 18:05:49 +0200

    WIP

--- resources/views/components/newsletter-form.blade.php
+++ resources/views/components/newsletter-form.blade.php
@@ -13,69 +13,71 @@ state([
 rules(['email' => 'required|email']);
 
 $subscribe = function () {
-    if ($this->done) {
-        return;
-    }
-
     $validated = $this->validate();
 
     app(Subscribe::class)->subscribe($validated['email']);
 
-    $this->done = true;
+    $this->dispatch('done', true);
 };
 
 ?>
 
 @volt('newsletter-form')
-    <aside id="newsletter" class="text-center">
-        @if ($done)
-            <x-icon-envelope class="h-24 mx-auto" />
-        @else
-            <x-icon-envelope-closed class="h-24 mx-auto" />
-        @endif
+    <aside
+        id="newsletter"
+        class="text-center"
+        x-data="{
+            title: 'I share what I learn, <span class=\'font-semibold text-transparent bg-gradient-to-r from-indigo-300 to-indigo-400 bg-clip-text\'>for&nbsp;free</span>.',
+            button: 'Join 400+ developers',
+            done: false,
+        }"
+        @subscribing="() => {
+            title = 'Getting everything ready…'
+            button = 'Subscribing…'
+        }"
+        @done="() => {
+            done = true
+            title = 'Done. Check your inbox!'
+            button = 'Thank you!'
+            $refs.field.blur()
+        }"
+    >
+        <x-icon-envelope class="h-24 mx-auto" x-cloak x-show="done" />
+        <x-icon-envelope-closed class="h-24 mx-auto" x-show="! done" />
 
         <p
-            @if (request()->routeIs('home'))
-                class="mt-6 text-2xl font-bold md:mt-8 md:text-3xl lg:text-4xl xl:text-5xl"
-            @else
-                class="mt-4 text-xl font-bold md:mt-6 md:text-2xl"
-            @endif
+            class="mt-6 text-2xl font-bold md:mt-8 md:text-3xl lg:text-4xl xl:text-5xl"
+            x-html="title"
             x-ref="title"
         >
-            @if ($done)
-                Done. Check your inbox!
-            @else
-                I share what I learn, <span class="font-semibold text-transparent bg-gradient-to-r from-indigo-300 to-indigo-400 bg-clip-text">for&nbsp;free</span>.
-            @endif
+            I share what I learn, <span class="font-semibold text-transparent bg-gradient-to-r from-indigo-300 to-indigo-400 bg-clip-text">for&nbsp;free</span>.
         </p>
 
         <form
-            class="sm:max-w-[480px] sm:mx-auto mt-4 md:mt-6"
-            @submit.prevent="$wire.subscribe(); $refs.button.textContent = 'Subscribing…'; $refs.title.textContent = 'Getting everything ready…'"
+            class="sm:max-w-[480px] sm:mx-auto mt-8 md:mt-10"
+            @submit.prevent="$wire.subscribe(); $dispatch('subscribing')"
         >
             @csrf
 
             <input
-                type="text"
+                type="email"
                 id="email"
                 wire:model="email"
                 placeholder="johndoe@example.com"
                 required
-                @if ($done) disabled @endif
+                :disabled="done"
+                x-ref="field"
                 class="w-full disabled:ring-1 disabled:ring-emerald-400 px-[.85rem] placeholder-gray-300 h-[44px] border-0 rounded shadow shadow-black/5 disabled:text-emerald-400 disabled:shadow-none transition-colors duration-500"
             />
 
             <button
-                class="bg-indigo-400 shadow-lg shadow-blue-700/20 h-[44px] font-bold rounded mt-2 px-8 text-white transition-colors duration-500
-                @if ($done) disabled:shadow-none disabled:bg-emerald-400 @endif"
-                @if ($done) disabled @endif
+                class="bg-indigo-400 shadow-lg shadow-blue-700/20 h-[44px] font-bold rounded mt-2 px-8 text-white transition-colors duration-500"
+                x-bind:class="{ 'disabled:shadow-none disabled:bg-emerald-400': done, }"
+                :disabled="done"
                 x-ref="button"
+                x-text="button"
             >
-                @if ($done)
-                    Thank you!
-                @else
-                    Join 400+ developers
-                @endif
+                Join 400+ developers
             </button>
         </form>
 
