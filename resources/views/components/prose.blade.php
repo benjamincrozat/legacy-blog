7864b41442384ff290ed28dcc723e3be89d44214
Benjamin Crozat <benjamincrozat@me.com>
2023-10-04 16:10:19 +0200

    WIP

--- resources/views/components/prose.blade.php
+++ resources/views/components/prose.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'prose break-words prose-a:text-indigo-400 prose-td:p-4 prose-th:p-4 max-w-none prose-pre:p-0 prose-pre:prose-code:p-4']) }}>
+<div {{ $attributes->merge(['class' => 'prose break-words prose-a:text-indigo-600 prose-td:p-4 prose-th:p-4 max-w-none prose-pre:p-0 prose-pre:prose-code:p-4']) }}>
     {{ $slot }}
 </div>
