4e0a3853ff1fac5b44a38325e8658fe90851b20a
Benjamin Crozat <benjamincrozat@me.com>
2023-10-08 10:58:35 +0200

    WIP

--- resources/views/components/dummy-store/layout.blade.php
+++ resources/views/components/dummy-store/layout.blade.php
@@ -3,6 +3,15 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    @if (app()->isProduction() && auth()->id() !== 1)
+        <script
+            defer src="https://api.pirsch.io/pirsch-extended.js"
+            id="pirschextendedjs"
+            data-code="5N2hIsUQsCVX1LQtvPdJ3AGwQZHGxtt5"
+            data-disable-page-views
+        ></script>
+    @endif
+
     <div class="container py-16">
         {{ $slot }}
     </div>
