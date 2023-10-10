7061c2223628172ea15541fb3cdab82d93c35fde
Benjamin Crozat <benjamincrozat@me.com>
2023-10-10 20:01:38 +0200

    WIP

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -1,4 +1,12 @@
-<div {{ $attributes->merge(['class' => 'grid items-center grid-cols-2 gap-4 sm:gap-6 md:gap-8 mt-8 lg:grid-cols-4 md:gap-12']) }}>
+<div {{
+    $attributes
+        ->merge(['class' => 'grid items-center grid-cols-2 gap-4 sm:gap-6 md:gap-8 mt-8 md:gap-12'])
+        ->when(
+            request()->routeIs('media-kit'),
+            fn ($attributes) => $attributes->merge(['class' => 'lg:grid-cols-5']),
+            fn ($attributes) => $attributes->merge(['class' => 'lg:grid-cols-4']),
+        )
+}}>
     <a
         href="https://beyondco.de/?utm_source=benjamincrozat&utm_medium=logo&utm_campaign=benjamincrozat"
         target="_blank"
@@ -24,6 +32,18 @@
             class="inline w-auto h-7 md:h-8"
         />
     </a>
+
+    @if (request()->routeIs('media-kit'))
+        <a
+            href="https://larajobs.com?utm_source=benjamincrozat&utm_medium=logo&utm_campaign=benjamincrozat"
+            target="_blank"
+            rel="noopener"
+            class="text-center"
+        >
+            <x-icon-larajobs class="inline h-6 md:h-7" />
+            <span class="sr-only">LaraJobs</span>
+        </a>
+    @endif
 
     <a
         href="https://ploi.io/?utm_campaign=sponsorship&utm_source=benjamincrozat.com&utm_medium=logo"
