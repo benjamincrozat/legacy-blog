bc5d9f7a679f7daad6eb41bc140c50c3aef00f50
Benjamin Crozat <benjamincrozat@me.com>
2023-10-24 15:37:28 +0200

    WIP

--- resources/views/openings/create.blade.php
+++ resources/views/openings/create.blade.php
@@ -1,17 +1,17 @@
 <x-app
     title="Post your job offer to 40,000 developers each month"
-    description="If you are hiring web developers, my blog can redirect a relevant audience to your job offer, today."
+    description="If you are hiring Laravel developers, my blog can redirect a relevant audience to your job offer, today."
 >
     <div class="container mt-16">
         <x-icon-people class="h-24 mx-auto" />
 
         <h1 class="!text-3xl md:!text-4xl/none mt-8 lg:!text-5xl/none font-bold text-center">
             Post your job offer
-            <span class="text-transparent bg-gradient-to-r from-indigo-300 to-indigo-400 bg-clip-text">to 40,000 web developers!</span>
+            <span class="text-transparent bg-gradient-to-r from-indigo-300 to-indigo-400 bg-clip-text">to 40,000 Laravel developers!</span>
         </h1>
 
         <h2 class="mt-2 text-center text-xl/tight md:text-2xl/tight lg:text-3xl/tight">
-            Hire the ideal developer <strong class="font-medium">faster</strong>.
+            Hire the ideal Laravel developer <strong class="font-medium">faster</strong>.
         </h2>
 
         <a
@@ -58,7 +58,7 @@
 
     <x-section id="numbers" class="container mt-24 md:mt-32 scroll-mt-4">
         <x-slot:title class="text-2xl sm:!text-3xl font-bold text-center">
-            Post your job offer to an ever growing amount of developers
+            Post your job offer to an ever growing amount of Laravel developers
         </x-slot:title>
 
         <div class="grid grid-cols-2 gap-16 mt-8 md:grid-cols-3">
@@ -68,7 +68,7 @@
                     <span x-text="Math.round(count).toLocaleString()">40,000</span>
                 </div>
 
-                <div>monthly developers passing by</div>
+                <div>monthly Laravel developers passing by</div>
             </div>
 
             <div class="col-span-1" x-data="{ count: 0, target: 63000 }" x-intersect.half="animateNumber">
@@ -140,9 +140,9 @@
         }
     </script>
 
-    <x-section id="about" class="container mt-32 mb-16 lg:max-w-screen-md">
+    <x-section class="container mt-32 mb-16 lg:max-w-screen-md">
         <x-slot:title class="text-2xl sm:!text-3xl font-bold text-center">
-            Why post your job offer on my blog
+            Why post your Laravel development job offers on&nbsp;my&nbsp;blog
         </x-slot:title>
 
         <x-prose class="mt-8 text-xl">
@@ -154,5 +154,42 @@
 
             <p><strong>People are always looking for new adventures and better work conditions.</strong> Take it as an opportunity to attract the ideal hire!”</p>
         </x-prose>
+
+        <x-button no-wire-navigate href="#" class="table px-6 mx-auto mt-8 text-white bg-orange-400">
+            Post your job offer
+        </x-button>
+    </x-section>
+
+    <x-section class="container mt-32 mb-16 lg:max-w-screen-md">
+        <x-slot:title class="text-2xl sm:!text-3xl font-bold text-center">
+            What to look for when hiring a dedicated Laravel developer?
+        </x-slot:title>
+
+        <x-prose class="mt-8 prose-h3:font-bold">
+            <h3 class="leading-tight">Check for real-world Laravel development experiences</h3>
+            <x-heroicon-s-check-circle class="float-right w-24 h-24 mt-1 mb-8 ml-8 text-green-500 md:w-32 md:h-32" />
+            <p>If you don't have time to coach inexperienced developers on Laravel, make sure they have real-world experience.</p>
+            <p><strong>People freshly out of school are certainly cheap, but they won't be able to be proficient on Laravel projects</strong> (of course, some passionate developers are the exception to this rule).</p>
+            <p>Here's what, in my opinion, makes a good candidate:</p>
+            <ol>
+                <li><strong>Make sure they have at least one successful experience with Laravel.</strong> It doesn't matter if this experience has been acquired working on open-source projects or in a company;</li>
+                <li><strong>Developers with tech culture and knowledge about the latest trends tend to be more curious</strong> and, therefore, willing to give everything they have to solve any problem you encounter with Laravel;</li>
+                <li><strong>A GitHub account is a good sign.</strong> Even when inactive, it can show at least a basic comprehension of open source, which reinforces the previous point.</li>
+            </ol>
+
+            <h3 class="leading-tight">Don't hire a senior Laravel developer to work on basic tasks</h3>
+            <x-icon-programmer class="float-right w-24 h-24 mt-1 mb-8 ml-8 text-green-500 md:w-32 md:h-32" />
+            <p><strong>A senior Laravel developer is expensive and for good reasons.</strong> Years of experience made them a master at what they do. <strong>You want them to work on something other than simple interface changes or bug fixes, or they'll get bored and leave as soon as possible.</strong></p>
+            <p>Senior Laravel developers want to work on complex issues that will make your project easier to maintain, more stable, and more profitable in the long run.</p>
+
+            <h3 class="leading-tight">Get a Laravel developer who's a good communicator</h3>
+            <x-icon-communication class="float-right w-24 h-24 mt-1 mb-8 ml-8 text-green-500 md:w-32 md:h-32" />
+            <p><strong>Communication is critical for any relationship.</strong> In a working environment, you need a clear view of the progression to plan what's next.</p>
+            <p>And you also need to know when things are stuck. Sometimes, problems are tough to solve, and deciding to get someone else involved as soon as possible to help you move forward is crucial.</p>
+        </x-prose>
+
+        <x-button no-wire-navigate href="#" class="table px-6 mx-auto mt-8 text-white bg-orange-400">
+            Post your job offer
+        </x-button>
     </x-section>
 </x-app>
