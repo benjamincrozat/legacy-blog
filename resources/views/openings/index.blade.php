d30a0ea2740811bd46a7c7f77f232d10df754106
Benjamin Crozat <benjamincrozat@me.com>
2023-10-24 18:19:45 +0200

    WIP

--- resources/views/openings/index.blade.php
+++ resources/views/openings/index.blade.php
@@ -46,7 +46,7 @@
             <p class="font-bold text-xl/tight md:text-2xl/tight">Business owners,<br /> find a developer, now!</p>
             <x-icon-interview class="float-right w-16 h-16 mt-1 mb-8 ml-8 text-green-500 md:mb-16 md:ml-16 md:w-24 md:h-24" />
             <p class="mt-4">My blog is visited by 40,000 Laravel developers each month. This is the right place to find your ideal candidate.</p>
-            <x-button href="{{ route('openings.create') }}" class="inline-block px-6 mt-8 text-white bg-orange-400 sm:text-lg">
+            <x-button href="{{ route('openings.create') }}" class="inline-block px-6 mt-8 text-base text-white bg-orange-400">
                 Get started
             </x-button>
         </div>
